03f531c6217ba1becf8545b8798928de1cb68867
Tiago Carvalho <1221124@isep.ipp.pt>
2024-10-17 21:29:27 +0100

    #5 UC003: Design - Update SD

--- obj/Debug/net7.0/DDDNetCore.AssemblyInfo.cs
+++ obj/Debug/net7.0/DDDNetCore.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DDDNetCore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c92f162453c023aec919ec7c29d188a674cae9c6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d1feeae03c5e3f9def141c2e9ffaa1b86e1f8cdd")]
 [assembly: System.Reflection.AssemblyProductAttribute("DDDNetCore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DDDNetCore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
