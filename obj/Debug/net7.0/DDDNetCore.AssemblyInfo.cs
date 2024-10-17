9e038ca888359e2d49ea04a019fcc7e3249660ee
Tiago Carvalho <1221124@isep.ipp.pt>
2024-10-17 20:16:31 +0100

    #2 Update Domain Model

--- obj/Debug/net7.0/DDDNetCore.AssemblyInfo.cs
+++ obj/Debug/net7.0/DDDNetCore.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DDDNetCore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3bf603a069f396a1a4ef3251b3906de8129ab6c0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ea8218e0d7a6dc715630f37f09e027133ceed934")]
 [assembly: System.Reflection.AssemblyProductAttribute("DDDNetCore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DDDNetCore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
