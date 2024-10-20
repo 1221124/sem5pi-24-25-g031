b25301266e4545d6464d0073baafc9f0755c3c90
1220786 <1220786@isep.ipp.pt>
2024-10-20 11:47:34 +0100

    #10 #11 #12 #13 Updated Patient Aggregate
    created class Gender

---
+++ src/Domain/Patient/Gender.cs
@@ -0,0 +1,24 @@
+namespace Domain.Patient
+{
+  public enum Gender
+  {
+    MALE,
+    FEMALE
+  }
+
+  public class GenderName
+  {
+    public static string Get(Gender gender)
+    {
+      switch(gender){
+        case Gender.FEMALE:
+          return "Female";
+        case Gender.MALE:
+          return "Male";
+        default:
+          throw new ArgumentException("Invalid Gender");
+      }
+    }
+  }
+}  
+
