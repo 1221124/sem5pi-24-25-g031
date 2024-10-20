a6855842e65a1b2d37d1a85c96c486171cbbca3a
1220786 <1220786@isep.ipp.pt>
2024-10-20 11:48:04 +0100

    #10 #11 #12 #13 Updated Patient Aggregate
    created class CreatingPatientDto

---
+++ src/Domain/Patient/CreatingPatientDto.cs
@@ -0,0 +1,29 @@
+using Domain.Shared;
+
+namespace Domain.Patient{
+
+    public class CreatingPatientDto{
+        public FullName FullName { get; set; }
+        public Name Name { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public Gender Gender { get; set; }
+        public MedicalRecordNumber MedicalRecordNumber { get; set; }
+        public ContactInformation ContactInformation { get; set; }
+        public List<MedicalConditions> MedicalConditions { get; set; }
+        public EmergencyContact EmergencyContact { get; set; }
+        //public AppointmentHistory AppointementHistory { get; set; }
+
+        public Patient(FullName fullName, Name name, DateTime dateOfBirth, Gender gender, MedicalRecordNumber medicalRecordNumber, ContactInformation contactInformation, MedicalConditions medicalConditions, EmergencyContact emergencyContact/*, AppointementHistory appointementHistory*/)
+        {
+          FullName = fullName;
+          Name = name;
+          DateOfBirth = dateOfBirth; 
+          Gender = gender;
+          MedicalRecordNumber = medicalRecordNumber;
+          ContactInformation = contactInformation;
+          MedicalConditions = medicalConditions;
+          EmergencyContact = emergencyContact;
+          //AppointmentHistory = appointmentHistory;
+        }
+    }
+}
