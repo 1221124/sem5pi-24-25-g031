fb3aed12e763dcac6e9664a96942bf7708efa8af
Tiago Carvalho <1221124@isep.ipp.pt>
2024-10-21 15:40:09 +0100

    #1 Update git ignore

--- obj/Debug/net7.0/DDDNetCore.AssemblyInfo.cs
+++ obj/Debug/net7.0/DDDNetCore.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DDDNetCore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+08ceeefd85fd34a6e5e0d9a72488949da9e7ef5e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8d55a6ad47a760c7b38b397a9c9919c05635d935")]
 [assembly: System.Reflection.AssemblyProductAttribute("DDDNetCore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DDDNetCore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
