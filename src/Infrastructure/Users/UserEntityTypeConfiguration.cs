0d0e515e8852a03b83aef2785f5c0cc35ca7211c
Tiago Carvalho <1221124@isep.ipp.pt>
2024-10-21 15:26:06 +0100

    #3 Update UserEntityTypeConfiguration

--- src/Infrastructure/Users/UserEntityTypeConfiguration.cs
+++ src/Infrastructure/Users/UserEntityTypeConfiguration.cs
@@ -9,7 +9,38 @@ namespace Infrastructure.Users
     {
         public void Configure(EntityTypeBuilder<User> builder)
         {
-            builder.HasKey(o => o.Id);
+            builder.HasKey(u => u.Id);
+
+            builder.OwnsOne(u => u.Username, username =>
+            {
+                username.Property(n => n.Value)
+                    .HasColumnName("Username")
+                    .IsRequired()
+                    .HasMaxLength(100);
+            });
+
+            builder.OwnsOne(u => u.Email, email =>
+            {
+                email.Property(e => e.Value)
+                    .HasColumnName("Email")
+                    .IsRequired()
+                    .HasMaxLength(100);
+            });
+
+            builder.Property(u => u.Role)
+                .IsRequired()
+                .HasConversion(
+                    v => RoleUtils.ToString(v),
+                    v => RoleUtils.FromString(v)
+                );
+
+            builder.Property(u => u.UserStatus)
+                .IsRequired()
+                .HasConversion(
+                    v => UserStatusUtils.ToString(v),
+                    v => UserStatusUtils.FromString(v)
+                )
+                .HasColumnName("UserStatus");
         }
     }
 }
