454857c5cc80497b4698628fd6ed807a2c9cdf7b
1220786 <1220786@isep.ipp.pt>
2024-10-21 12:36:32 +0100

    #8 #9 #10 #11 PatientRepository
    new class patient repository

---
+++ src/Infrastructure/Patients/PatientRepository.cs
@@ -0,0 +1,29 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Domain.Patients;
+using Domain.Shared;
+using Infrastructure.Shared;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Infrastructure.Patients
+{
+    public class PatientRepository : BaseRepository<Patient, PatientId>, IPatientRepository
+    {
+
+        public PatientRepository(SARMDbContext context):base(context.Patients)
+        {
+
+        }
+
+        public void Configure(EntityTypeBuilder<Patient> builder)
+        {
+            // cf. https://www.entityframeworktutorial.net/efcore/fluent-api-in-entity-framework-core.aspx
+            
+            //builder.ToTable("Categories", SchemaNames.DDDSample1);
+            builder.HasKey(b => b.Id);
+            //builder.Property<bool>("_active").HasColumnName("Active");
+        }
+    }
+}
