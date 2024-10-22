f23452e1f1fafa030e3932fbadda1d5b74cc2de2
1220786 <1220786@isep.ipp.pt>
2024-10-22 10:21:14 +0100

    #10 #11 #12 #13 Infrastructure patients
    Added code to PatientEntityTypeConfiguration to be able to create the tables in the DB

--- src/Infrastructure/Patients/PatientEntityTypeConfiguration.cs
+++ src/Infrastructure/Patients/PatientEntityTypeConfiguration.cs
@@ -1,6 +1,11 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 using Domain.Patients;
+using Domain.Shared;
+using Google.Type;
+using DateTime = System.DateTime;
+using PhoneNumber = Domain.Shared.PhoneNumber;
+
 // using Domain.Shared;
 
 namespace Infrastructure.Patients
@@ -9,11 +14,79 @@ namespace Infrastructure.Patients
     {
         public void Configure(EntityTypeBuilder<Patient> builder)
         {
-            // cf. https://www.entityframeworktutorial.net/efcore/fluent-api-in-entity-framework-core.aspx
+            builder.HasKey(p => p.Id);
             
-            // builder.ToTable("OperationType", SchemaNames.g031);
-            builder.HasKey(b => b.Id);
-            // builder.Property<Status>("Status").HasColumnName("Active").IsRequired();
+            builder.OwnsOne(p => p.FullName, name =>
+            {
+                name.Property(n => n.FirstName)
+                    .HasColumnName("FirstName")
+                    .IsRequired()
+                    .HasMaxLength(100);
+                name.Property(n => n.LastName)
+                    .HasColumnName("LastName")
+                    .IsRequired()
+                    .HasMaxLength(100);
+            });
+
+            builder.Property(p => p.DateOfBirth)
+                .IsRequired()
+                .HasColumnName("DateOfBirth")
+                .HasConversion(
+                    v => v.ToString("yyyy-MM-dd"), 
+                    v => DateTime.Parse(v) 
+                );
+
+                
+
+            builder.Property(p => p.Gender)
+                .HasColumnName("Gender")
+                .HasConversion(
+                    v => GenderUtils.ToString(v),
+                    v => GenderUtils.FromString(v)
+                );
+            
+            /*
+            builder.Property(p => p.MedicalRecordNumber)
+                .HasColumnName("MedicalRecordNumber");
+                
+            */
+            
+            builder.OwnsOne(p=> p.ContactInformation, contact =>
+            {
+                contact.Property(c => c.PhoneNumber)
+                    .HasColumnName("PhoneNumber")
+                    .IsRequired()
+                    .HasConversion(
+                        v=> v.ToString(),
+                        v=> PhoneNumber.FromString(v))
+                    .HasMaxLength(100);
+                contact.Property(c => c.Email)
+                    .HasColumnName("Email")
+                    .IsRequired()
+                    .HasMaxLength(100);
+            });
+            
+            builder.OwnsMany(p => p.MedicalConditions, medicalConditions =>
+            {
+                medicalConditions.Property(m => m.Condition)
+                    .HasColumnName("MedicalCondition")
+                    .HasMaxLength(100);
+            });
+            
+            builder.OwnsOne(p => p.EmergencyContact, emergencyContact =>
+            {
+                emergencyContact.Property(e => e.Number)
+                    .HasColumnName("EmergencyContactPhoneNumber")
+                    .HasConversion(
+                        v=> v.ToString(),
+                        v=> PhoneNumber.FromString(v))
+                    .HasMaxLength(100);
+            });
+            
+            
+            
+            
+                
         }
     }
 }
