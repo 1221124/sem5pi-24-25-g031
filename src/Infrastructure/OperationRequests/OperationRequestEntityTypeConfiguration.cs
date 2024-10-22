23719098c8b14be2edb2c5f4e82533189405cab3
1220784 <1220784@isep.ipp.pt>
2024-10-22 10:42:25 +0100

    #18 #19 #20 #21 OperationTypeConfiguration implemented

--- src/Infrastructure/OperationRequests/OperationRequestEntityTypeConfiguration.cs
+++ src/Infrastructure/OperationRequests/OperationRequestEntityTypeConfiguration.cs
@@ -1,18 +1,51 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 using Domain.OperationRequests;
+using System;
 
 namespace Infrastructure.OperationRequests
 {
     internal class OperationRequestEntityTypeConfiguration : IEntityTypeConfiguration<OperationRequest>
     {
         public void Configure(EntityTypeBuilder<OperationRequest> builder)
-        {
-            // cf. https://www.entityframeworktutorial.net/efcore/fluent-api-in-entity-framework-core.aspx
-            
-            //builder.ToTable("OperationType", SchemaNames.DDDSample1);
+        {           
             builder.HasKey(b => b.Id);
-            //builder.Property<bool>("_active").HasColumnName("Active");
+
+            builder.Property(p => p.PatientId)
+                .IsRequired()
+                .HasColumnName("PatientId");
+
+            builder.Property(p => p.DoctorId)
+                .IsRequired()
+                .HasColumnName("StaffId");
+
+            builder.Property(p => p.OperationTypeId)
+                .IsRequired()
+                .HasColumnName("OperationTypeId");
+
+            builder.Property(p => p.DeadlineDate)
+                .IsRequired()
+                .HasColumnName("DeadlineDate")
+                .HasConversion(
+                    v => v.ToString("yyyy-MM-dd"),
+                    v => DateTime.Parse(v)
+                );
+
+            builder.Property(p => p.Priority)
+                .IsRequired()
+                .HasColumnName("Priority")
+                .HasConversion(
+                    v => PriorityUtils.ToString(v),
+                    v => PriorityUtils.FromString(v)
+                );
+            
+            builder.Property(p => p.Status)
+                .IsRequired()
+                .HasColumnName("Status")
+                .HasConversion(
+                    v => RequestStatusUtils.ToString(v),
+                    v => RequestStatusUtils.FromString(v)
+                );
         }
     }
 }
