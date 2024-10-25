938a35523be63a164864fd56ba7ab0f0aa4bd797
1220784 <1220784@isep.ipp.pt>
2024-10-25 23:01:32 +0100

    #1 UsersSession update to handle Authorizations

--- src/Domain/UsersSession/IUserSessionRepository.cs
+++ src/Domain/UsersSession/IUserSessionRepository.cs
@@ -6,5 +6,6 @@ namespace Domain.UsersSession
 {
     public interface IUserSessionRepository : IRepository<UserSession, UserSessionId>
     {
+        Task<UserSession?> GetByEmailAsync(Email admin);
     }
 }
--- src/Domain/UsersSession/UserSession.cs
+++ src/Domain/UsersSession/UserSession.cs
@@ -3,21 +3,29 @@ using Domain.Users;
 using Domain.UsersSession;
 
 namespace Domain.UsersSession{
-public class UserSession : Entity<UserSessionId>, IAggregateRoot
-{
-    public UserId UserId { get; set; }
-    public Email Email { get; set; }
-    public Role Role { get; set; }
+    public class UserSession : Entity<UserSessionId>, IAggregateRoot
+    {
+        public UserId UserId { get; set; }
+        public Email Email { get; set; }
+        public Role Role { get; set; }
+        public DateTime ExpiresIn { get; set; }
 
-    public UserSession()
-    {
+        public UserSession()
+        {
+        }
+        
+        public UserSession(UserId userId, Email email, Role role)
+        {
+            Id = new UserSessionId(Guid.NewGuid());
+            UserId = userId;
+            Email = email;
+            Role = role;
+            ExpiresIn = DateTime.Now.AddMinutes(60);
+        }
+
+        public bool IsExpired()
+        {
+            return DateTime.Now >= ExpiresIn;
+        }
     }
-    public UserSession(UserId userId, Email email, Role role)
-    {
-        Id = new UserSessionId(Guid.NewGuid());
-        UserId = userId;
-        Email = email;
-        Role = role;
-    }
-}
 }
