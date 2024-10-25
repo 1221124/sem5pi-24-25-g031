390e0653c7df0278471302ae8c5c286b1d339fed
1220784 <1220784@isep.ipp.pt>
2024-10-25 21:51:40 +0100

    #8 Migrations Update to new UserSession Update

--- Migrations/20241025205015_UpdateUserSessionFinal.Designer.cs
+++ Migrations/20241025205015_UpdateUserSessionFinal.Designer.cs
@@ -4,6 +4,7 @@ using Infrastructure;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace DDDNetCore.Migrations
 {
     [DbContext(typeof(SARMDbContext))]
-    partial class SARMDbContextModelSnapshot : ModelSnapshot
+    [Migration("20241025205015_UpdateUserSessionFinal")]
+    partial class UpdateUserSessionFinal
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
