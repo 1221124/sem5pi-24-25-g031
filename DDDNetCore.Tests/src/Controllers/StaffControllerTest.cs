02c2f0accd3ebd3fd61a6797a7953a7f0569e10f
1200347 <1200347@isep.ipp.pt>
2024-10-27 01:30:31 +0000

    #1 Tests folder updated

---
+++ DDDNetCore.Tests/src/Controllers/StaffControllerTest.cs
@@ -0,0 +1,17 @@
+﻿using Controllers;
+using JetBrains.Annotations;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DDDNetCore.Tests.src.Controllers;
+
+[TestClass]
+[TestSubject(typeof(StaffController))]
+public class StaffControllerTest
+{
+
+    [TestMethod]
+    public void METHOD()
+    {
+        
+    }
+}
