1e98e2e9c545839284f8eaff0a834b98329520dd
1220784 <1220784@isep.ipp.pt>
2024-10-31 09:38:51 +0000

    #18 #19 #20 #21 OperationRequestsController updated

--- sarm/src/Controllers/OperationRequestsController.cs
+++ sarm/src/Controllers/OperationRequestsController.cs
@@ -3,18 +3,19 @@ using Microsoft.AspNetCore.Mvc;
 using Domain.OperationRequests;
 using Domain.OperationTypes;
 using Domain.Shared;
+using IOperationRequestService = DDDNetCore.Domain.OperationRequests.IOperationRequestService;
 
-namespace Controllers
+namespace DDDNetCore.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
     public class OperationRequestController : ControllerBase
     {
-        private readonly int pageSize = 2;
-        private readonly OperationRequestService _operationRequestService;
-        private readonly DbLogService _logService;
+        private readonly int _pageSize = 2;
+        private readonly IOperationRequestService _operationRequestService;
+        private readonly IDbLogService _logService;
 
-        public OperationRequestController(OperationRequestService operationRequestService, DbLogService logService)
+        public OperationRequestController(IOperationRequestService operationRequestService, IDbLogService logService)
         {
             _operationRequestService = operationRequestService;
             _logService = logService;
@@ -22,20 +23,28 @@ namespace Controllers
         
         // GET api/operationrequest
         [HttpGet]
-        public async Task<ActionResult<IEnumerable<OperationRequest>>> Get(int pageNumber)
+        public virtual async Task<ActionResult<IEnumerable<OperationRequestDto>>> Get([FromQuery] string? pageNumber)
         {
-            try{
+            try
+            {
                 var operationRequests = await _operationRequestService.GetAllAsync();
+                
                 if(operationRequests == null)
                     return NotFound();
+
+                if (pageNumber != null)
+                {
+                    var paginated = operationRequests
+                        .Skip((int.Parse(pageNumber)) *
+                              _pageSize) // Pula os primeiros itens de acordo com o número da página e o tamanho da página
+                        .Take(_pageSize) // Seleciona a quantidade especificada de itens para a página atual
+                        .ToList(); // Converte o resultado em uma lista para fácil manipulação
+
+                    return Ok(paginated);
+                }
+
+                return Ok(operationRequests);
                 
-                var paginated = operationRequests
-                    .Skip((pageNumber - 1) * pageSize) // Pula os primeiros itens de acordo com o número da página e o tamanho da página
-                    .Take(pageSize)                    // Seleciona a quantidade especificada de itens para a página atual
-                    .ToList();                         // Converte o resultado em uma lista para fácil manipulação
-
-                return Ok(paginated);
-
             }catch(Exception ex){
                 return BadRequest("Error: " + ex.Message);
             }
@@ -43,7 +52,7 @@ namespace Controllers
 
         // GET api/operationrequest/5
         [HttpGet("id/{id}")]
-        public async Task<ActionResult<OperationTypeDto>> GetById(Guid id)
+        public virtual async Task<ActionResult<OperationRequestDto>> GetById(Guid id)
         {
             try
             {
@@ -63,7 +72,7 @@ namespace Controllers
         
         // GET api/operationrequest/patientname
         [HttpGet("patientname/{fullName}")]
-        public async Task<ActionResult<IEnumerable<OperationRequest>>> GetByPatientName(string fullName, int pageNumber)
+        public virtual async Task<ActionResult<IEnumerable<OperationRequestDto>>> GetByPatientName(string fullName, string? pageNumber)
         {
             try{
                 if(fullName == null)
@@ -80,16 +89,23 @@ namespace Controllers
 
                 var operationRequests = await _operationRequestService.GetByPatientNameAsync(name);
 
-                if(operationRequests == null)
+                if(operationRequests == null || operationRequests.Count == 0)
                     return NotFound();
 
-                var paginated = operationRequests
-                    .Skip((pageNumber - 1) * pageSize) // Pula os primeiros itens de acordo com o número da página e o tamanho da página
-                    .Take(pageSize)                    // Seleciona a quantidade especificada de itens para a página atual
-                    .ToList();  
 
-                return Ok(paginated);
+                if (pageNumber != null)
+                {
+                    var paginated = operationRequests
+                        .Skip((int.Parse(pageNumber) - 1) *
+                              _pageSize) // Pula os primeiros itens de acordo com o número da página e o tamanho da página
+                        .Take(_pageSize) // Seleciona a quantidade especificada de itens para a página atual
+                        .ToList();
 
+                    return Ok(paginated);
+                }
+
+                return Ok(operationRequests);
+                
             }catch(Exception ex){
                 return BadRequest("Error: " + ex.Message);
             }
@@ -97,7 +113,7 @@ namespace Controllers
         
         // GET api/operationrequest/operationtype
         [HttpGet("operationtype/{operationType}")]
-        public async Task<ActionResult<IEnumerable<OperationRequest>>> GetByOperationType(string operationType, int pageNumber)
+        public virtual async Task<ActionResult<IEnumerable<OperationRequestDto>>> GetByOperationType(string operationType, string? pageNumber)
         {
             try{
                 if(operationType == null)
@@ -110,12 +126,18 @@ namespace Controllers
                 if(operationRequests == null)
                     return NotFound();
 
-                var paginated = operationRequests
-                    .Skip((pageNumber - 1) * pageSize) // Pula os primeiros itens de acordo com o número da página e o tamanho da página
-                    .Take(pageSize)                    // Seleciona a quantidade especificada de itens para a página atual
-                    .ToList();  
+                if (pageNumber != null)
+                {
+                    var paginated = operationRequests
+                        .Skip((int.Parse(pageNumber) - 1) *
+                              _pageSize) // Pula os primeiros itens de acordo com o número da página e o tamanho da página
+                        .Take(_pageSize) // Seleciona a quantidade especificada de itens para a página atual
+                        .ToList();
 
-                return Ok(paginated);
+                    return Ok(paginated);
+                }
+
+                return Ok(operationRequests);
 
             }
             catch(Exception ex)
@@ -126,7 +148,7 @@ namespace Controllers
 
         // GET api/operationrequest/status
         [HttpGet("status/{status}")]
-        public async Task<ActionResult<IEnumerable<OperationRequest>>> GetByStatus(string status, int pageNumber)
+        public virtual async Task<ActionResult<IEnumerable<OperationRequestDto>>> GetByStatus(string status, string? pageNumber)
         {
             try{
                 if(status == null)
@@ -139,12 +161,18 @@ namespace Controllers
                 if(operationRequests == null)
                     return NotFound();
 
-                var paginated = operationRequests
-                    .Skip((pageNumber - 1) * pageSize) // Pula os primeiros itens de acordo com o número da página e o tamanho da página
-                    .Take(pageSize)                    // Seleciona a quantidade especificada de itens para a página atual
-                    .ToList();
+                if (pageNumber != null)
+                {
+                    var paginated = operationRequests
+                        .Skip((int.Parse(pageNumber) - 1) *
+                              _pageSize) // Pula os primeiros itens de acordo com o número da página e o tamanho da página
+                        .Take(_pageSize) // Seleciona a quantidade especificada de itens para a página atual
+                        .ToList();
 
-                return Ok(paginated);
+                    return Ok(paginated);
+                }
+                
+                return Ok(operationRequests);
 
             }
             catch(Exception ex)
@@ -153,10 +181,9 @@ namespace Controllers
             }
         }
         
-        
         // POST: api/OperationTypes
         [HttpPost]
-        public async Task<ActionResult<OperationRequestDto>> Create([FromBody] CreatingOperationRequestDto dto)
+        public virtual async Task<ActionResult<OperationRequestDto>> Create([FromBody] CreatingOperationRequestDto dto)
         {
             var entity = EntityType.OperationRequest;
             var log = DbLogType.Create;
@@ -165,7 +192,7 @@ namespace Controllers
             {
                 if (dto == null)
                 {
-                    _logService.LogAction(entity, log,"Creating Operation Type DTO cannot be null");
+                    await _logService.LogAction(entity, log,"Creating Operation Type DTO cannot be null");
                     return BadRequest("Creating Operation Type DTO cannot be null");
                 }
 
@@ -173,7 +200,7 @@ namespace Controllers
 
                 if (operationRequest == null)
                 {
-                    _logService.LogAction(EntityType.OperationRequest, DbLogType.Create,
+                    await _logService.LogAction(EntityType.OperationRequest, DbLogType.Create,
                         "Operation Request was not created.");
                     return BadRequest("Operation Request was not created.");
                 }
@@ -182,18 +209,18 @@ namespace Controllers
             }
             catch (Exception ex)
             {
-                _logService.LogAction(entity, log, "Error in Create: " + ex.Message);
+                await _logService.LogAction(entity, log, "Error in Create: " + ex.Message);
                 return BadRequest("Error in Create: " + ex.Message);
             }
         }
 
         //PUT api/operationrequest/update
         [HttpPut("update")]
-        public async Task<ActionResult<OperationRequestDto>> Update([FromBody] UpdatingOperationRequestDto dto)
+        public virtual async Task<ActionResult<OperationRequestDto>> Update([FromBody] UpdatingOperationRequestDto dto)
         {
             try{
                 if (dto == null) {
-                    _logService.LogAction(EntityType.OperationRequest, DbLogType.Update,"Operation request data is required.");
+                    await _logService.LogAction(EntityType.OperationRequest, DbLogType.Update,"Operation request data is required.");
                     return BadRequest("Operation request data is required.");
                 }
 
@@ -205,14 +232,14 @@ namespace Controllers
                 return Ok("Operation request updated successfully.");
 
             }catch(Exception ex){
-                //_DBLogService.LogError(OperationRequestEntityType, ex.Message);
+                await _logService.LogAction(EntityType.OperationRequest, DbLogType.Update, ex.Message);
                 return BadRequest("Error in Update: " + ex.Message);
             }
         }
 
         // DELETE api/operationrequest/5
         [HttpDelete("{id}")]
-        public async Task<ActionResult<OperationRequestDto>> Delete(Guid id)
+        public virtual async Task<ActionResult<OperationRequestDto>> Delete(Guid id)
         {
             try{   
                 var operationRequestDto = await _operationRequestService.DeleteAsync(new OperationRequestId(id));
@@ -224,7 +251,7 @@ namespace Controllers
             }
             catch(Exception ex)
             {
-                _logService.LogAction(EntityType.OperationType, DbLogType.Delete, ex.Message);
+                await _logService.LogAction(EntityType.OperationType, DbLogType.Delete, ex.Message);
                 return BadRequest("Error in Delete: " + ex.Message); 
             }
         }
