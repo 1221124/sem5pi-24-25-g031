0e685945de41f099f86b8077c3ed4b9d3f0b31fd
1200347 <1200347@isep.ipp.pt>
2024-11-06 17:21:39 +0000

    #41 Environment updated with staffUrl

--- ui/src/environments/environment.ts
+++ ui/src/environments/environment.ts
@@ -1,14 +1,15 @@
 export const environment = {
-    production: false,
-    tokenUrl: 'https://dev-sagir8s22k2ehmk0.us.auth0.com/oauth/token',
-    loginUrl: 'https://dev-sagir8s22k2ehmk0.us.auth0.com/authorize?audience=https://api.sarmg031.com&response_type=code&client_id=ZkqvMdGFLKP5d2DOlKCj8pnqDVihkffn&redirect_uri=http://localhost:4200/callback&scope=openid%20profile%20email&prompt=login',
-    usersApiUrl: 'http://localhost:5500/api/Users',
-    operationRequests: 'http://localhost:5500/api/OperationRequests',
-    authConfig: {
-      clientId: 'ZkqvMdGFLKP5d2DOlKCj8pnqDVihkffn',
-      clientSecret: 'NnTGmyVIeaoTO9SfHdPRs5wVMpQJrdq_fbkUlkwxy5xfCJiARpsxrGZMY9LnBeSR',
-      redirectUri: 'http://localhost:4200/callback',
-      authDomain: 'https://dev-sagir8s22k2ehmk0.us.auth0.com/',
-      audience: 'https://api.sarmg031.com'
-    }
-  };
+  production: false,
+  tokenUrl: 'https://dev-sagir8s22k2ehmk0.us.auth0.com/oauth/token',
+  loginUrl: 'https://dev-sagir8s22k2ehmk0.us.auth0.com/authorize?audience=https://api.sarmg031.com&response_type=code&client_id=ZkqvMdGFLKP5d2DOlKCj8pnqDVihkffn&redirect_uri=http://localhost:4200/callback&scope=openid%20profile%20email&prompt=login',
+  usersApiUrl: 'http://localhost:5500/api/Users',
+  operationRequests: 'http://localhost:5500/api/OperationRequests',
+  staffs: 'http://localhost:5500/api/Staff',
+  authConfig: {
+    clientId: 'ZkqvMdGFLKP5d2DOlKCj8pnqDVihkffn',
+    clientSecret: 'NnTGmyVIeaoTO9SfHdPRs5wVMpQJrdq_fbkUlkwxy5xfCJiARpsxrGZMY9LnBeSR',
+    redirectUri: 'http://localhost:4200/callback',
+    authDomain: 'https://dev-sagir8s22k2ehmk0.us.auth0.com/',
+    audience: 'https://api.sarmg031.com'
+  }
+};
