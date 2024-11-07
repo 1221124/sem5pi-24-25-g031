1f58b09481a0dcf20f03e6581e55bd9e1b4ffe31
1220784 <1220784@isep.ipp.pt>
2024-11-07 19:40:30 +0000

    #45 operation.requests.service updated

--- ui/src/app/services/operation-requests/operation-requests.service.ts
+++ ui/src/app/services/operation-requests/operation-requests.service.ts
@@ -1,6 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
+import { environment } from '../../../environments/environment';
+import { catchError, Observable, tap, throwError } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -12,9 +14,13 @@ export class OperationRequestsService {
     private http: HttpClient, private router: Router
   ) {}
 
-  post(staffIdDto: string, patientIdDto: string, operationTypeIdDto: string, deadlineDateDto: Date, priorityDto: string) {
-    
-    const apiUrl = 'http://localhost:5500/operationRequests';
+  post(
+    staffIdDto: string, 
+    patientIdDto: string, 
+    operationTypeIdDto: string, 
+    deadlineDateDto: Date, 
+    priorityDto: string
+  ){    
     
     const dto = { //creatingOperationRequestDto
       staffId: staffIdDto,
@@ -24,14 +30,14 @@ export class OperationRequestsService {
       priority: priorityDto
     };
 
+    console.log("Submitting to:", environment.operationRequests);
 
-    this.http.post(apiUrl, dto).subscribe(
-      _response => {
-          this.message = 'Operation request submitted successfully!';
-      },
-      _error => {
-          this.message = 'Error submitting operation request. Please try again.';
-      }
+    this.http.post(environment.operationRequests, dto).subscribe(
+      response => 
+        console.log('Operation request submitted successfully:', response),
+      error => 
+        console.error('Error submitting operation request:', error)
     );
+
   };
 }
