ec596559ee00200d10316153853e07ab202365e9
1220786 <1220786@isep.ipp.pt>
2024-11-07 08:25:05 +0000

    #37 app routes

--- ui/src/app/app.routes.ts
+++ ui/src/app/app.routes.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { HomeComponent } from './components/home/home.component';
 import { StaffsComponent } from './components/staffs/staffs.component';
 import { OperationRequestsComponent } from './components/operation-requests/operation-requests.component';
+import { PatientsComponent }  from './components/patients/patients.component';
 
 export const routes: Route[] = [
   { path: '', component: HomeComponent },
   { path: 'callback', component: AuthCallbackComponent },
   { path: 'staffs', component: StaffsComponent },
-  { path: 'operationRequests', component: OperationRequestsComponent }
+  { path: 'operationRequests', component: OperationRequestsComponent },
+  { path: 'patients', component: PatientsComponent }
 ];
 
 @NgModule({
