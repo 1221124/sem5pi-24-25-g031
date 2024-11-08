905ab6104a499903f23fe83d5f44993fe9e1305f
1220784 <1220784@isep.ipp.pt>
2024-11-08 16:09:21 +0000

    #45 environment.ts updated

--- ui/src/environments/environment.ts
+++ ui/src/environments/environment.ts
@@ -1,10 +1,12 @@
+import { HttpHeaders } from "@angular/common/http";
+
 export const environment = {
   production: false,
   homeUrl: 'http://localhost:4200',
   tokenUrl: 'https://dev-sagir8s22k2ehmk0.us.auth0.com/oauth/token',
   loginUrl: 'https://dev-sagir8s22k2ehmk0.us.auth0.com/authorize?audience=https://api.sarmg031.com&response_type=code&client_id=ZkqvMdGFLKP5d2DOlKCj8pnqDVihkffn&redirect_uri=http://localhost:4200/callback&scope=openid%20profile%20email&prompt=login',
   usersApiUrl: 'http://localhost:5500/api/Users',
-  operationRequests: 'http://localhost:5500/api/operationRequests',
+  operationRequests: 'http://localhost:5500/api/OperationRequest',
   staffs: 'http://localhost:5500/api/Staff',
   patients: 'http://localhost:5500/api/Patients',
   authConfig: {
@@ -14,4 +16,10 @@ export const environment = {
     authDomain: 'https://dev-sagir8s22k2ehmk0.us.auth0.com/',
     audience: 'https://api.sarmg031.com'
   }
+};
+
+export const httpOptions = {
+  headers: new HttpHeaders({
+    'Content-Type': 'application/json'
+  })
 };
