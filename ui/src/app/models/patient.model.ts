88bd100df5eb225044ea13238137f3f2e035b96a
1220784 <1220784@isep.ipp.pt>
2024-11-11 19:51:56 +0000

    #1 patient-model updated; token-response-model implemented

--- ui/src/app/models/patient.model.ts
+++ ui/src/app/models/patient.model.ts
@@ -1,6 +1,7 @@
 // src/app/models/patient.model.ts
 
 export interface Patient {
+  email: string;
   medicalRecordNumber: string;
-  }
+}
   
---
+++ ui/src/app/models/token-response.ts
@@ -0,0 +1,4 @@
+export interface TokenResponse {
+    access_token: string;
+    id_token: string;
+}
