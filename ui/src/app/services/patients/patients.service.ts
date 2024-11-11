7d3c07deaf9ad2d5a12cb21e42854843e3bbda78
1220786 <1220786@isep.ipp.pt>
2024-11-11 20:01:21 +0000

    #39 FE Patient Service
    deleted patients method

--- ui/src/app/services/patients/patients.service.ts
+++ ui/src/app/services/patients/patients.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import {HttpClient, HttpHeaders} from '@angular/common/http';
 import { Router } from '@angular/router';
-import { environment } from '../../../environments/environment';
+import {environment, httpOptions} from '../../../environments/environment';
 import {Observable} from 'rxjs';
 
 @Injectable({
@@ -98,6 +98,11 @@ export class PatientsService {
 
   updatePatient(patient: any): Observable<any> {
     return this.http.put(`${this.apiUrl}`, patient);
+  }
+
+  deletePatient(patientId: any): Observable<any>{
+    console.log("Deletion Patient ID:", patientId);
+    return this.http.delete(`${this.apiUrl}/admin/${patientId}`, httpOptions);
   }
 
 }
