02565d335b9522d880492939fc8e7d5f584dfce0
Tiago Carvalho <1221124@isep.ipp.pt>
2024-11-12 20:56:21 +0000

    #1 Update App Component  tests

--- ui/src/app/app.component.spec.ts
+++ ui/src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'ui' title`, () => {
+  it(`should have the 'SARM G031 Web Application' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('ui');
+    expect(app.title).toEqual('SARM G031 Web Application');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, ui');
+    expect(compiled.querySelector('h1')?.textContent).toContain('SARM G031 Web Application');
   });
 });
