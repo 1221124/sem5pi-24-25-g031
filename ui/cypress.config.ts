1bdbd9410e369ce5b93f871e9538ad1e09681049
Tiago Carvalho <1221124@isep.ipp.pt>
2024-11-15 00:38:56 +0000

    #49 #50 #51 #52 Add E2E Operation Types tests (not done)

--- ui/cypress.config.ts
+++ ui/cypress.config.ts
@@ -2,6 +2,10 @@ import { defineConfig } from "cypress";
 
 export default defineConfig({
   e2e: {
+    baseUrl: "http://localhost:4200",
+    env: {
+      operationTypesUrl: "/OperationTypes",
+    },
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
