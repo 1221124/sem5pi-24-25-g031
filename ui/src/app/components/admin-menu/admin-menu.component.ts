8d8d23ad9b746f345b03f44350493a49f1b5b0e0
1220786 <1220786@isep.ipp.pt>
2024-11-20 12:03:25 +0000

    #64 3D visualization Module
    added button to admin menu to add the site of the 3d
    
    Co-Authored-By: 1200347 <83037736+beatrizsilva5@users.noreply.github.com>

--- ui/src/app/components/admin-menu/admin-menu.component.ts
+++ ui/src/app/components/admin-menu/admin-menu.component.ts
@@ -24,4 +24,8 @@ export class AdminMenuComponent implements OnInit {
   navigateTo(path: string) {
     this.router.navigateByUrl('/admin/' + path), { replaceUrl: true };
   }
+
+  navigateTo3D(): void {
+    window.location.href = 'http://localhost:63343/3DVisualizationModule/Basic_Thumb_Raiser/Thumb_Raiser.html?_ijt=fpr539t4ojcdr8oac0bkehc8j1&_ij_reload=RELOAD_ON_SAVE';
+  }
 }
