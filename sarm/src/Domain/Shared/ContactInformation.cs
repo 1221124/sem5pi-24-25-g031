152362656f79142fcadc21497a411546a36db649
1200347 <1200347@isep.ipp.pt>
2024-11-21 14:43:05 +0000

    #14 #15 #16 #17 Contact Information Unit Test creataed

--- sarm/src/Domain/Shared/ContactInformation.cs
+++ sarm/src/Domain/Shared/ContactInformation.cs
@@ -6,8 +6,33 @@ namespace Domain.Shared
         public PhoneNumber PhoneNumber { get; set; }
         public ContactInformation(Email email, PhoneNumber phoneNumber)
         {
+            if (email == null) 
+            {
+                throw new ArgumentNullException(nameof(email), "Email cannot be null");
+            }
+        
+            if (phoneNumber == null) 
+            {
+                throw new ArgumentNullException(nameof(phoneNumber), "PhoneNumber cannot be null");
+            }
+            
             Email = email;
             PhoneNumber = phoneNumber;
+        }
+        
+        public override bool Equals(object obj)
+        {
+            if (obj is ContactInformation other)
+            {
+                return Email.Equals(other.Email) && PhoneNumber.Equals(other.PhoneNumber);
+            }
+            return false;
+        }
+
+        public override int GetHashCode()
+        {
+            // Combina os valores de hash de Email e PhoneNumber
+            return HashCode.Combine(Email, PhoneNumber);
         }
 
         /*public ContactInformation()
