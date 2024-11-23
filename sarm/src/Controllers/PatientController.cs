4a9ddaab2f3e69acdd84ac0f59867481e3a787a2
1220786 <1220786@isep.ipp.pt>
2024-11-23 16:19:08 +0000

    #33 #34 Patient Controller
    changed path

--- sarm/src/Controllers/PatientController.cs
+++ sarm/src/Controllers/PatientController.cs
@@ -237,7 +237,7 @@ namespace DDDNetCore.Controllers
             }
         }
 
-        // PUT: api/Patient/patientouUser
+        // PUT: api/Patient/
         [HttpPut]
         //[Authorize(Roles = "Admin,Patient")]
         public async Task<ActionResult<PatientDto>> AdminUpdate(UpdatingPatientDto dto)
