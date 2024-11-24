32d17b00c16b635fe83279266e6817392fbe297e
1200347 <1200347@isep.ipp.pt>
2024-11-24 21:14:04 +0000

    #32 Fix errors in Unit Tests related to httpClient

--- ui/src/app/services/admin-users/admin-users.service.spec.ts
+++ ui/src/app/services/admin-users/admin-users.service.spec.ts
@@ -1,12 +1,17 @@
 import { TestBed } from '@angular/core/testing';
 
 import { AdminUsersService } from './admin-users.service';
+import {provideHttpClient, withFetch} from '@angular/common/http';
 
 describe('AdminUsersService', () => {
   let service: AdminUsersService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [
+        provideHttpClient(withFetch())
+      ],
+    });
     service = TestBed.inject(AdminUsersService);
   });
 
