ded3af2de8076268d5095646985c12573c165536
Tiago Carvalho <1221124@isep.ipp.pt>
2024-12-02 11:33:29 +0000

    #1 Add apikey

--- sarm/Program.cs
+++ sarm/Program.cs
@@ -33,12 +33,12 @@ var builder = WebApplication.CreateBuilder(args);
 
 AppSettings.Initialize(builder.Configuration);
 
-var apiKey = builder.Configuration.GetValue<string>("SendinBlue:ApiKey");
+// var apiKey = builder.Configuration.GetValue<string>("SendinBlue:ApiKey");
 
-if (string.IsNullOrEmpty(apiKey))
-{
-    throw new InvalidOperationException("API Key for SendinBlue is missing.");
-}
+// if (string.IsNullOrEmpty(apiKey))
+// {
+//     throw new InvalidOperationException("API Key for SendinBlue is missing.");
+// }
 
 builder.Services.AddMemoryCache();
 
@@ -130,11 +130,11 @@ builder.Services.AddScoped<PrologService>();
 builder.Services.AddScoped<PrologIntegrationService>();
 builder.Services.AddScoped<PrologService>();
 
-builder.Services.AddSingleton<IEmailService>(new EmailService("sarmg031@gmail.com", apiKey));
+builder.Services.AddSingleton<IEmailService>(new EmailService("sarmg031@gmail.com", "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"));
 
 builder.Services.AddTransient<PatientCleanupService>();
 
-builder.Services.AddSingleton(new EmailService("sarmg031@gmail.com", apiKey));
+builder.Services.AddSingleton(new EmailService("sarmg031@gmail.com", "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"));
 
 builder.Services.AddHttpClient<IAMService>();
 
