05da631022125ee25570e88220b62c9bf6bb467a
Tiago Carvalho <1221124@isep.ipp.pt>
2024-12-03 11:43:29 +0000

    #1 Add Swagger to production development (temporary)

--- sarm/Program.cs
+++ sarm/Program.cs
@@ -165,17 +165,18 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseDeveloperExceptionPage();
-    app.UseSwagger();
-    app.UseSwaggerUI(c =>
-    {
-        c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
-        c.RoutePrefix = string.Empty;
-    });
 }
 else
 {
     app.UseHsts();
 }
+
+app.UseSwagger();
+app.UseSwaggerUI(c =>
+{
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+    c.RoutePrefix = string.Empty;
+});
 
 app.UseHttpsRedirection();
 app.UseRouting();
