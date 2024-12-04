1a4f81566db3fffac9f73f98b0e9182301285860
1200347 <1200347@isep.ipp.pt>
2024-12-04 18:34:38 +0000

    #1 Appointments service updated

--- ui/src/app/services/appointments/appointments.service.spec.ts
+++ ui/src/app/services/appointments/appointments.service.spec.ts
@@ -1,12 +1,15 @@
 import { TestBed } from '@angular/core/testing';
 
 import { AppointmentsService } from './appointments.service';
+import {provideHttpClient, withFetch} from '@angular/common/http';
 
 describe('AppointmentsService', () => {
   let service: AppointmentsService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({ providers: [
+        provideHttpClient(withFetch())
+      ],});
     service = TestBed.inject(AppointmentsService);
   });
 
