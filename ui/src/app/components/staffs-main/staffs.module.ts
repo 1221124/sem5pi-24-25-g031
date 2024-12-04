c0fb0c1f3c8510b56550cd111e8d655b8457ca28
1200347 <1200347@isep.ipp.pt>
2024-12-04 18:32:46 +0000

    #41 #42 #43 #44 Staff module and route created

---
+++ ui/src/app/components/staffs-main/staffs.module.ts
@@ -0,0 +1,24 @@
+﻿import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {AppModule} from '../../app.module';
+import {AppComponent} from '../../app.component';
+import {FormsModule} from '@angular/forms';
+import {StaffsComponent} from "./staffs/staffs.component";
+import {ListStaffsComponent} from "./list-staffs/list-staffs.component";
+
+@NgModule({
+  declarations: [
+  ],
+  imports: [
+    AppComponent,
+    CommonModule,
+    FormsModule,
+    StaffsComponent,
+    ListStaffsComponent
+  ],
+  exports: [
+    StaffsComponent,
+    ListStaffsComponent
+  ]
+})
+export class StaffModule { }
---
+++ ui/src/app/components/staffs-main/staffs.route.ts
@@ -0,0 +1,13 @@
+﻿import { Route, RouterModule } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { StaffsMainComponent } from './staffs/staffs.component';
+
+export const routes: Route[] = [
+  { path: '', component: StaffsMainComponent },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
