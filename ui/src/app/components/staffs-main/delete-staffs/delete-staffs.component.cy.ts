34f85657640efeb97f9257792460db4c5008162f
1200347 <1200347@isep.ipp.pt>
2024-12-04 18:28:29 +0000

    #43 Creation component of deactivated staffs

---
+++ ui/src/app/components/staffs-main/delete-staffs/delete-staffs.component.cy.ts
@@ -0,0 +1,7 @@
+import { DeleteStaffsComponent } from './delete-staffs.component'
+
+describe('DeleteStaffsComponent', () => {
+  it('should mount', () => {
+    cy.mount(DeleteStaffsComponent)
+  })
+})
---
+++ ui/src/app/components/staffs-main/delete-staffs/delete-staffs.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-delete-staffs',
+  templateUrl: './delete-staffs.component.html',
+  styleUrl: './delete-staffs.component.css'
+})
+export class DeleteStaffsComponent {
+
+}
