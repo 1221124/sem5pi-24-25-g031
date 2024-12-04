448ee4a16498a0e7ee783fa992d9740dacd1c12c
1200347 <1200347@isep.ipp.pt>
2024-12-04 18:24:20 +0000

    #44 Creation of Slot Appointment

---
+++ ui/src/app/components/staffs-main/slot-appointment-staffs/slot-appointment-staffs.component.cy.ts
@@ -0,0 +1,7 @@
+import { SlotAppointmentStaffsComponent } from './slot-appointment-staffs.component'
+
+describe('SlotAppointmentStaffsComponent', () => {
+  it('should mount', () => {
+    cy.mount(SlotAppointmentStaffsComponent)
+  })
+})
---
+++ ui/src/app/components/staffs-main/slot-appointment-staffs/slot-appointment-staffs.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-slot-appointment-staffs',
+  templateUrl: './slot-appointment-staffs.component.html',
+  styleUrl: './slot-appointment-staffs.component.css'
+})
+export class SlotAppointmentStaffsComponent {
+
+}
