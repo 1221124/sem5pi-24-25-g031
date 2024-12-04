227158c8bf72d6a6fbf7f9f7efbe433c236872a9
1200347 <1200347@isep.ipp.pt>
2024-12-04 18:27:37 +0000

    #44 Creation component of list staffs

---
+++ ui/src/app/components/staffs-main/list-staffs/list-staffs.component.cy.ts
@@ -0,0 +1,7 @@
+import { ListStaffsComponent } from './list-staffs.component'
+
+describe('ListStaffsComponent', () => {
+  it('should mount', () => {
+    cy.mount(ListStaffsComponent)
+  })
+})
---
+++ ui/src/app/components/staffs-main/list-staffs/list-staffs.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-list-staffs',
+  templateUrl: './list-staffs.component.html',
+  styleUrl: './list-staffs.component.css',
+  standalone: true,
+})
+export class ListStaffsComponent {
+
+}
