072801aa49839d2fd37b17399cd45a11bd1767eb
1220784 <1220784@isep.ipp.pt>
2024-12-06 14:51:59 +0000

    #45 #46 #47 #48 Operation Requests routing implemented

---
+++ ui/src/app/components/operation-requests-main/operation-requests.route.ts
@@ -0,0 +1,19 @@
+﻿import {NgModule} from '@angular/core';
+import {Route, RouterModule} from '@angular/router';
+import {CreateOperationRequestComponent} from './create-operation-requests/create-operation-requests.component';
+import {UpdateOperationRequestsComponent} from './update-operation-requests/update-operation-requests.component';
+import {DeleteOperationRequestsComponent} from './delete-operation-requests/delete-operation-requests.component';
+
+export const routes: Route[] = [
+  {path: 'create', component: CreateOperationRequestComponent},
+  {path: 'update', component: UpdateOperationRequestsComponent},
+  {path: 'delete', component: DeleteOperationRequestsComponent},
+
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class OperationRequestsRoute {
+}
