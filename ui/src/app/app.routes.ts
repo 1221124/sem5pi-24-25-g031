68dab340aaad10b3d47b2e66c3ac7f58b5459116
1220784 <1220784@isep.ipp.pt>
2024-12-06 14:52:31 +0000

    #1 app.route.ts fixed

--- ui/src/app/app.routes.ts
+++ ui/src/app/app.routes.ts
@@ -3,36 +3,40 @@ import { AuthCallbackComponent } from './components/auth-callback/auth-callback.
 import { NgModule } from '@angular/core';
 import { HomeComponent } from './components/home/home.component';
 import { StaffsComponent } from './components/staffs-main/staffs/staffs.component';
-import { OperationRequestsComponent } from './components/operation-requests/operation-requests.component';
-import { PatientComponent } from './components/patient/patient-main/patient.component';
 import { AdminPatientsComponent } from './components/admin-patients/admin-patients.component';
 import { OperationTypesComponent } from './components/operation-types/operation-types.component';
 import { AdminMenuComponent } from './components/admin-menu/admin-menu.component';
 import { AdminUsersComponent } from './components/admin-users/admin-users.component';
 import { DoctorMenuComponent } from './components/doctor-menu/doctor-menu.component';
-import { NurseMenuComponent } from './components/nurse-menu/nurse-menu.component';
-import { TechnicianMenuComponent } from './components/technician-menu/technician-menu.component';
 import { PrologComponent } from './components/prolog/prolog.component';
 import { AppointmentsComponent } from './components/appointments/appointments.component';
-import { VerifyEmailComponent } from './components/verify-email/verify-email.component';
+import {
+  OperationRequestsComponent
+} from './components/operation-requests-main/operation-requests/operation-requests.component';
 
 export const routes: Route[] = [
   { path: '', component: HomeComponent },
   { path: 'callback', component: AuthCallbackComponent },
   { path: 'admin/staffs', component: StaffsComponent },
-  { path: 'doctor/operationRequests', component: OperationRequestsComponent },
   { path: 'admin/appointments', component: AppointmentsComponent },
   { path: 'admin/operationTypes', component: OperationTypesComponent },
   { path: 'admin/patients', component: AdminPatientsComponent },
   { path: 'admin/users', component: AdminUsersComponent },
   { path: 'admin/prolog', component: PrologComponent },
   { path: 'admin', component: AdminMenuComponent },
-  { path: 'doctor', component: DoctorMenuComponent },
-  { path: 'patient', component: PatientComponent },
-  { path: 'nurse', component: NurseMenuComponent },
-  { path: 'technician', component: TechnicianMenuComponent},
-  { path: 'verify-email', component: VerifyEmailComponent}
-];
+  {
+    path: 'doctor',
+    component: DoctorMenuComponent,
+    children: [
+      {
+        path: 'operation-requests',
+        component: OperationRequestsComponent,
+        loadChildren: () =>
+          import('./components/operation-requests-main/operation-requests.module')
+          .then((m) => m.OperationRequestsModule)
+      }
+    ]}
+  ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
