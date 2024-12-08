a39a731d0bc876416273cabca0889e57efb02822
1200347 <1200347@isep.ipp.pt>
2024-12-08 00:44:24 +0000

    #41 Create Staff working

--- ui/src/app/components/staffs-main/create-staffs/create-staffs.component.ts
+++ ui/src/app/components/staffs-main/create-staffs/create-staffs.component.ts
@@ -1,33 +1,124 @@
-import { Component } from '@angular/core';
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
 import {Staff} from '../../../models/staff.model';
+import {OperationRequest} from '../../../models/operation-request.model';
+import {FormsModule} from '@angular/forms';
+import {NgFor, NgIf} from '@angular/common';
+import {StaffsService} from '../../../services/staffs/staffs.service';
+import {AuthService} from '../../../services/auth/auth.service';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-create-staffs',
   templateUrl: './create-staffs.component.html',
+  standalone: true,
+  imports: [
+    FormsModule,
+    NgFor,
+    NgIf
+  ],
   styleUrl: './create-staffs.component.css'
 })
-export class CreateStaffsComponent {
-  staff: Staff = new Staff();
-  specializations: string[] = ['Specialization 1', 'Specialization 2'];
-  roles: string[] = ['Role 1', 'Role 2'];
+export class CreateStaffsComponent implements OnInit {
+  @Input() staff!: Staff;
 
-  isCreateModalOpen = false;
+  @Output() createRequestEvent = new EventEmitter<Staff>();
+  @Output() closeModalEvent = new EventEmitter<unknown>();
 
-  openModal() {
-    this.isCreateModalOpen = true;
+  specializations: string[] = [];
+  roles: string[] = [];
+  names: string[] = [];
+  emails: string[] = [];
+
+  firstNameTouched = false;
+  lastNameTouched = false;
+  emailTouched = false;
+  phoneNumberTouched = false;
+  specializationTouched = false;
+  roleTouched = false;
+
+  accessToken: string = '';
+
+  constructor(private staffService: StaffsService, private authService: AuthService, private router: Router) {
   }
 
-  closeModal() {
-    this.isCreateModalOpen = false;
+  ngOnInit(): void {
+
+    if (!this.staff) {
+      this.staff = {
+        Id: '',
+        FullName: {FirstName: '', LastName: ''},
+        licenseNumber: '',
+        specialization: '',
+        staffRole: '',
+        ContactInformation: {Email: '', PhoneNumber: ''},
+        status: '',
+        SlotAppointment: [{Start: '', End: ''}],
+        SlotAvailability: [{Start: '', End: ''}],
+      };
+    }
+
+    if (!this.authService.isAuthenticated()) {
+      this.authService.updateMessage('You are not authenticated or are not an admin! Please login...');
+      this.authService.updateIsError(true);
+      setTimeout(() => {
+        this.router.navigate(['']);
+      }, 3000);
+
+      return;
+    }
+
+    this.accessToken = this.authService.getToken() as string;
+    if (!this.authService.extractRoleFromAccessToken(this.accessToken)?.toLowerCase().includes('admin')) {
+      this.authService.updateMessage(
+        'You are not authenticated or are not an admin! Redirecting to login...'
+      );
+      this.authService.updateIsError(true);
+      setTimeout(() => {
+        this.router.navigate(['']);
+      }, 3000);
+
+      return;
+    }
+
+    this.staffService.getStaffRoles().then((data) => {
+      this.roles = data;
+    });
+
+    this.staffService.getSpecializations().then((data) => {
+      this.specializations = data;
+    });
+
+  }
+
+
+  closeCreateModal() {
+    this.closeModalEvent.emit();
   }
 
   submitRequest() {
-    // Logic for submitting the staff data
-    this.staffCreated.emit(this.staff);
-    this.closeModal();
+     this.createRequestEvent.emit(this.staff);
   }
 
   clearForm() {
-    this.staff = new Staff(); // Clear the form
+    this.staff = {
+      Id: '',
+      FullName: { FirstName: '', LastName: '' },
+      licenseNumber: '',
+      specialization: '',
+      staffRole: '',
+      ContactInformation: { Email: '', PhoneNumber: '' },
+      status: '',
+      SlotAppointment: [{ Start: '', End: '' }],
+      SlotAvailability: [{ Start: '', End: '' }],
+    };
+
+    this.firstNameTouched = false;
+    this.lastNameTouched = false;
+    this.emailTouched = false;
+    this.phoneNumberTouched = false;
+    this.specializationTouched = false;
+    this.roleTouched = false;
   }
+
+
 }
