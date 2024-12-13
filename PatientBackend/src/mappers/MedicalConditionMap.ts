c2982a58ad4510b98d7a06f9ea96053a13ddafee
1220784 <1220784@isep.ipp.pt>
2024-12-13 18:11:57 +0000

    #76 #78 Medical Condition Map updated
    Following base project

--- PatientBackend/src/mappers/MedicalConditionMap.ts
+++ PatientBackend/src/mappers/MedicalConditionMap.ts
@@ -1,33 +1,49 @@
+import { UniqueEntityID } from "../core/domain/UniqueEntityID";
+import { CommonSymptom } from "../domain/medical-condition/CommonSyptom";
 import { MedicalCondition } from "../domain/medical-condition/MedicalCondition";
+import { Description } from "../domain/shared/Description";
+import { ICD11Code } from "../domain/shared/ICD11Code";
+import { Name } from "../domain/shared/Name";
 import { CreatingMedicalConditionDto } from "../dto/medical-condition/CreatingMedicalConditionDto";
 import { MedicalConditionDto } from "../dto/medical-condition/MedicalConditionDto";
 
 export class MedicalConditionMap {
-    static toDto(medicalCondition: MedicalCondition): MedicalConditionDto {
-      return new MedicalConditionDto(
-        medicalCondition.code, 
-        medicalCondition.name, 
-        medicalCondition.description, 
-        medicalCondition.commonSymptoms
-      );
+    public  static toDto(medicalCondition: MedicalCondition): MedicalConditionDto {
+      return {
+        code: medicalCondition.code, 
+        name: medicalCondition.name, 
+        description: medicalCondition.description, 
+        commonSymptoms: medicalCondition.commonSymptoms
+      } as MedicalConditionDto;
     }
   
-    static toEntity(medicalConditionDto: MedicalConditionDto): MedicalCondition {
-      return new MedicalCondition(
-        medicalConditionDto.code, 
-        medicalConditionDto.name, 
-        medicalConditionDto.description, 
-        medicalConditionDto.commonSymptoms
-      );
+    public static async toDomain(raw: any): Promise<MedicalCondition> {
+      const codeOrError = ICD11Code.create(raw.code);
+      const nameOrError = Name.create(raw.name);
+      const descriptionOrError = Description.create(raw.description);
+      const commonSymptomsOrError = raw.commonSymptom.map((symptom: any) => CommonSymptom.create(symptom));
+
+      const medicalConditionOrError = MedicalCondition.create({
+        code: codeOrError.getValue(),
+        name: nameOrError.getValue(),
+        description: descriptionOrError.getValue(),
+        commonSymptoms: commonSymptomsOrError.map(
+          (symptomOrError: any) => symptomOrError.getValue())
+      }, new UniqueEntityID(raw.medicalConditionId));
+
+      medicalConditionOrError.isFailure ? console.log(medicalConditionOrError.error) : '';
+
+      return medicalConditionOrError.isSuccess ? medicalConditionOrError.getValue() : null;
     }
-  
-    static toEntityFromCreating(CreatingMedicalConditionDto: CreatingMedicalConditionDto): MedicalCondition {
-      return new MedicalCondition(
-        CreatingMedicalConditionDto.code, 
-        CreatingMedicalConditionDto.name, 
-        CreatingMedicalConditionDto.description, 
-        CreatingMedicalConditionDto.commonSymptoms
-      );
+
+    public static toPersistence (medicalCondition: MedicalCondition): any {
+      return {
+        medicalConditionId: medicalCondition.id.toString(),
+        code: medicalCondition.code.value,
+        name: medicalCondition.name.value,
+        description: medicalCondition.description.value,
+        commonSymptoms: medicalCondition.commonSymptoms.map(symptom => symptom.value)
+      };
     }
   }
   
