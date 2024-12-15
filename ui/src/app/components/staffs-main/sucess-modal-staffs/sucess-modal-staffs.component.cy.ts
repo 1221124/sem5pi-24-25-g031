be84ef35babc6e25baeb9ece6a31203856b742e1
1200347 <1200347@isep.ipp.pt>
2024-12-15 17:17:32 +0000

    #41 #42 #43 #44 Success modal component added

---
+++ ui/src/app/components/staffs-main/sucess-modal-staffs/sucess-modal-staffs.component.cy.ts
@@ -0,0 +1,7 @@
+import { SucessModalStaffsComponent } from './sucess-modal-staffs.component'
+
+describe('SucessModalStaffsComponent', () => {
+  it('should mount', () => {
+    cy.mount(SucessModalStaffsComponent)
+  })
+})
