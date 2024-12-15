8fade7b5d9ae1fd6233f8e86542fa2b6b41b58ff
1200347 <1200347@isep.ipp.pt>
2024-12-15 00:27:01 +0000

    #42 Componet update staffs updated

--- ui/src/app/components/staffs-main/update-staffs/update-staffs.component.ts
+++ ui/src/app/components/staffs-main/update-staffs/update-staffs.component.ts
@@ -1,28 +1,34 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core';
 import {Staff} from '../../../models/staff.model';
+import {FormsModule} from '@angular/forms';
+import {CommonModule} from '@angular/common';
 
 @Component({
   selector: 'app-update-staffs',
   templateUrl: './update-staffs.component.html',
+  standalone: true,
+  imports: [
+    FormsModule,
+    CommonModule
+  ],
   styleUrl: './update-staffs.component.css'
 })
+
 export class UpdateStaffsComponent {
-  @Input() isEditModalOpen: boolean = false;
-  @Input() staff: Staff;
-  @Input() specializations: string[] = [];
-  @Input() isEditMode: boolean = false;
-  @Output() onCloseModal = new EventEmitter<void>();
-  @Output() onSubmit = new EventEmitter<Staff>();
+  @Input() staff!: Staff;
+  @Input() specializations!: string[];
+
+  @Output() update = new EventEmitter<Staff>();
+  @Output() closeModalEvent = new EventEmitter<unknown>();
 
   closeModal() {
-    this.onCloseModal.emit();
+    this.closeModalEvent.emit();
   }
 
   saveStaff() {
-    this.onSubmit.emit(this.staff);
+    console.log('Updated request:', this.staff);
+
+    this.update.emit(this.staff);
   }
 
-  submitRequest() {
-    console.log('Submitting staff data', this.staff);
-  }
 }
