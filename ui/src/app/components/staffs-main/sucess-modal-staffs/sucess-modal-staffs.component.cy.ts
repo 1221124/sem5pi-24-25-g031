d85cc41aa05ff9e72bf8f24203d4ac445cd30f7a
1200347 <1200347@isep.ipp.pt>
2024-12-15 19:02:12 +0000

    #44 Success Modal Staffs Component deleted

--- ui/src/app/components/staffs-main/sucess-modal-staffs/sucess-modal-staffs.component.cy.ts
+++
@@ -1,7 +0,0 @@
-import { SucessModalStaffsComponent } from './sucess-modal-staffs.component'
-
-describe('SucessModalStaffsComponent', () => {
-  it('should mount', () => {
-    cy.mount(SucessModalStaffsComponent)
-  })
-})
