add7a1f78fe97937fc9d7b5a58edd9ed5f2891d6
1220786 <1220786@isep.ipp.pt>
2024-12-18 11:35:53 +0000

    #74 Create Allergies
    created AllergyService and iterface

---
+++ PatientBackend/src/services/AllergyService.ts
@@ -0,0 +1,34 @@
+import {Inject, Service} from "typedi";
+import config from "../../config";
+import IAllergyService from "./IServices/IAllergyService";
+import IAllergyRepo from "./IRepos/IAllergyRepo";
+import {CreatingAllergyDto} from "../dto/allergy/CreatingAllergyDto";
+
+import {Result} from "../core/logic/Result";
+import {AllergyDto} from "../dto/allergy/AllergyDto";
+import {AllergyMap} from "../mappers/AllergyMap";
+
+@Service()
+
+export default class AllergyService implements IAllergyService {
+    constructor(
+        @Inject(config.repos.allergy.name) private allergyRepo: IAllergyRepo
+    ) {}
+    
+    public async createAllergy(dto: CreatingAllergyDto): Promise<Result<AllergyDto>> {
+        try {
+            const creatingAllergy = AllergyMap.toDomain(dto);
+
+            if (!creatingAllergy) {
+                return Result.fail<AllergyDto>(creatingAllergy);
+            }
+
+            await this.allergyRepo.save(await creatingAllergy);
+            
+            const allergyDTO = AllergyMap.toDto(await creatingAllergy);
+            return Result.ok<AllergyDto>(allergyDTO);
+        } catch (error) {
+            return Result.fail<AllergyDto>(error);
+        }
+    }
+}
