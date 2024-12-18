2ef1d1e97d0cc80fa9bed7e22e8a587dae148c7d
1220786 <1220786@isep.ipp.pt>
2024-12-18 11:35:30 +0000

    #74 Create Allergies
    created allergyScheme to persistence

---
+++ PatientBackend/src/persistence/schemas/allergySchema.ts
@@ -0,0 +1,28 @@
+import mongoose from "mongoose";
+import {IAllergyPersistence} from "../../dataschema/IAllergyPersistence";
+
+const AllergySchema = new mongoose.Schema(
+    {
+        id: {
+            type: String,
+            required: true
+        },
+        name: {
+            type: String,
+            required: [true, 'Please enter name'],
+            index: true,
+        },
+        code: {
+            type: String,
+            required: [true, 'Please enter code'],
+            index: true,
+        },
+        description: {
+            type: String,
+            required: [true, 'Please enter description'],
+            index: true,
+        },
+    }
+);
+
+export default mongoose.model<IAllergyPersistence & mongoose.Document>('Allergy', AllergySchema);
