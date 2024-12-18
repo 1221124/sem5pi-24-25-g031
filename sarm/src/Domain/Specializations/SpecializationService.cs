484007947136e5ce23e7b78ff9b07e0f4b31fe16
1200347 <1200347@isep.ipp.pt>
2024-12-18 16:51:24 +0000

    #83 Create SpecializationService class

---
+++ sarm/src/Domain/Specializations/SpecializationService.cs
@@ -0,0 +1,40 @@
+﻿using Domain.Shared;
+
+namespace DDDNetCore.Domain.Specializations;
+
+public class SpecializationService
+{
+    private readonly ISpecializationRepository _repository;
+    private readonly IUnitOfWork _unitOfWork;
+
+    public SpecializationService(ISpecializationRepository repository, IUnitOfWork unitOfWork)
+    {
+        _repository = repository;
+        _unitOfWork = unitOfWork;
+    }
+
+    public async Task<List<Specialization>> GetAllAsync()
+    {
+        return await _repository.GetAllAsync();
+    }
+
+    public async Task<SpecializationDto> GetByIdAsync(SpecializationId id)
+    {
+        try
+        {   
+            var specialization = await _repository.GetByIdAsync(id);
+
+            if (specialization == null)
+            {
+                return null;
+            }
+            
+            return SpecializationMapper.ToDto(specialization);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            throw;
+        }
+    }
+}
