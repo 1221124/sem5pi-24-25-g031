a156440b7fb6bab653d9443db15461af3d016bc1
1220784 <1220784@isep.ipp.pt>
2024-12-18 19:40:34 +0000

    #1 Project Node Modules update

--- PatientBackend/build/src/domain/patient-medical-record/PatientMedicalRecord.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.PatientMedicalRecord = void 0;
-class PatientMedicalRecord {
-}
-exports.PatientMedicalRecord = PatientMedicalRecord;
-//# sourceMappingURL=PatientMedicalRecord.js.map
