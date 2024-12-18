33795bec5a3473c866663af5a9f4432112dff013
1220786 <1220786@isep.ipp.pt>
2024-12-18 11:34:40 +0000

    #74 Create Allergies
    created Allergy Dto for creating and the usual dto

---
+++ PatientBackend/src/dto/allergy/CreatingAllergyDto.ts
@@ -0,0 +1,15 @@
+import {ICD11Code} from "../../domain/shared/ICD11Code";
+import {Name} from "../../domain/shared/Name";
+import {Description} from "../../domain/shared/Description";
+
+export class CreatingAllergyDto {
+    code: ICD11Code;
+    name: Name;
+    description: Description;
+    
+    constructor(code: ICD11Code, name: Name, description: Description) {
+        this.code = code;
+        this.name = name;
+        this.description = description;
+    }
+}
