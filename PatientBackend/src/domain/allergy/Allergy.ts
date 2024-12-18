874b98fa08cbcf585744b12ffc2b5a07bc16c8f3
1220786 <1220786@isep.ipp.pt>
2024-12-18 11:34:00 +0000

    #74 Create Allergies
    created domain allergy

--- PatientBackend/src/domain/allergy/Allergy.ts
+++ PatientBackend/src/domain/allergy/Allergy.ts
@@ -1,15 +1,63 @@
 import { Description } from "../shared/Description";
 import { ICD11Code } from "../shared/ICD11Code";
 import { Name } from "../shared/Name";
+import {Aggregate} from "mongoose";
+import {AggregateRoot} from "../../core/domain/AggregateRoot";
+import {UniqueEntityID} from "../../core/domain/UniqueEntityID";
+import {AllergyId} from "./AllergyId";
+import {Result} from "../../core/logic/Result";
+import {Guard} from "../../core/logic/Guard";
 
-export class Allergy {
-    private code: ICD11Code;
-    private name: Name;
-    private description: Description;
+interface AllergyProps {
+    code: ICD11Code;
+    name: Name;
+    description: Description;
+}
 
-    constructor(code: ICD11Code, name: Name, description: Description) {
-        this.code = code;
-        this.name = name;
-        this.description = description;
+export class Allergy extends AggregateRoot<AllergyProps>{
+    get id(): UniqueEntityID {
+        return AllergyId.caller(this.id);
     }
+    
+    get allergyId(): AllergyId {
+        return AllergyId.caller(this.id);
+    }
+    
+    get code(): ICD11Code {
+        return this.props.code;
+    }
+    
+    get name(): Name {
+        return this.props.name;
+    }
+    
+    get description(): Description {
+        return this.props.description;
+    }
+    
+    private constructor(props: AllergyProps, id?: UniqueEntityID) {
+        super(props, id);
+    }
+    
+    public static create(props: AllergyProps, id?: UniqueEntityID): Result<Allergy> {
+        
+        const guardedProps = [
+            { argument: props.code, argumentName: 'code' },
+            { argument: props.name, argumentName: 'name' },
+            { argument: props.description, argumentName: 'description' }
+        ];
+        
+        const guardResult = Guard.againstNullOrUndefinedBulk(guardedProps);
+        
+        if (!guardResult.succeeded) {
+            return Result.fail<Allergy>(guardResult.message);
+        }else{
+            const allergy = new Allergy({
+                ...props
+            }, id);
+            
+            return Result.ok<Allergy>(allergy);
+        }
+    }
+   
 }
