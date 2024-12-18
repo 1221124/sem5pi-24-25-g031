efbf449e0e596d25a67dbd7d9e6f556074115894
1200347 <1200347@isep.ipp.pt>
2024-12-18 16:50:48 +0000

    #83 Create SpecializationMapper class

---
+++ sarm/src/Domain/Specializations/SpecializationMapper.cs
@@ -0,0 +1,42 @@
+﻿using System.Xml;
+using Domain.Shared;
+
+namespace DDDNetCore.Domain.Specializations;
+
+public class SpecializationMapper
+{
+    public static CreatingSpecializationDto ToCreating(
+        string name, string description, bool IsAvailableForSurgeries
+    )
+    {
+        return new CreatingSpecializationDto(
+            new Name(name),
+            new Description(description)
+        );
+    }
+
+    public static Specialization ToEntity(SpecializationDto dto)
+    {
+        return new Specialization(
+            new SpecializationId(dto.Id),
+            dto.SNOMEDCTCode,
+            dto.Name,
+            dto.Description
+        );
+    }
+
+    public static SpecializationDto ToDto(Specialization entity)
+    {
+        return new SpecializationDto(
+            entity.Id.AsGuid(),
+            entity.SNOMEDCTCode,
+            entity.Name,
+            entity.Description
+        );
+    }
+
+    public static List<SpecializationDto> ToDtoList(List<Specialization> entities)
+    {
+        return entities.Select(entity => ToDto(entity)).ToList();
+    }
+}
