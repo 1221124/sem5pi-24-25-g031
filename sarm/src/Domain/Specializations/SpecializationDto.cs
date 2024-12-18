f4bd8800a7091124f3265cae612eb2d5a8c68c48
1200347 <1200347@isep.ipp.pt>
2024-12-18 16:51:00 +0000

    #83 Create SpecializationDto class

---
+++ sarm/src/Domain/Specializations/SpecializationDto.cs
@@ -0,0 +1,19 @@
+﻿using Domain.Shared;
+
+namespace DDDNetCore.Domain.Specializations;
+
+public class SpecializationDto
+{
+    public Guid Id { get; set; }
+    public SNOMEDCTCode SNOMEDCTCode { get; set; }
+    public Name Name { get; set; }
+    public Description Description { get; set; }
+
+    public SpecializationDto(Guid id, SNOMEDCTCode snomedctCode, Name name, Description description)
+    {
+        Id = id;
+        SNOMEDCTCode = snomedctCode;
+        Name = name;
+        Description = description;
+    }
+}
