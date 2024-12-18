ab7f41f40a0bcf984a52a0768fa477e40a205212
Tiago Carvalho <1221124@isep.ipp.pt>
2024-12-18 11:23:58 +0000

    #88 Inserting in agenda according to presence

--- PlanningModule/lapr5/knowledge_base/kb-20260810.pl
+++ PlanningModule/lapr5/knowledge_base/kb-20260810.pl
@@ -1,11 +1,11 @@
-agenda_staff(d20241,20260810,[(100,180,ap1),(1080,1140,ap2)]).
-agenda_staff(d20242,20260810,[(100,180,ap1),(1080,1140,ap2)]).
-agenda_staff(d20243,20260810,[(100,180,ap1),(1080,1140,ap2)]).
-agenda_staff(d20244,20260810,[(100,180,ap1),(1080,1140,ap2)]).
-agenda_staff(n20241,20260810,[(100,180,ap1),(1080,1140,ap2)]).
-agenda_staff(n20242,20260810,[(100,180,ap1),(1080,1140,ap2)]).
-agenda_staff(n20243,20260810,[(100,180,ap1),(1080,1140,ap2)]).
-agenda_staff(t20241,20260810,[(100,180,ap1),(1080,1140,ap2)]).
+agenda_staff(d20241,20260810,[(100,235,ap1),(1080,1215,ap2)]).
+agenda_staff(d20242,20260810,[(100,235,ap1),(1080,1215,ap2)]).
+agenda_staff(d20243,20260810,[(100,235,ap1),(1080,1215,ap2)]).
+agenda_staff(d20244,20260810,[(100,235,ap1),(1080,1215,ap2)]).
+agenda_staff(n20241,20260810,[(100,235,ap1),(1080,1215,ap2)]).
+agenda_staff(n20242,20260810,[(100,235,ap1),(1080,1215,ap2)]).
+agenda_staff(n20243,20260810,[(100,235,ap1),(1080,1215,ap2)]).
+agenda_staff(t20241,20260810,[(100,235,ap1),(1080,1215,ap2)]).
 
 timetable(d20241,20260810,(0,1440)).
 timetable(d20242,20260810,(0,1440)).
@@ -26,6 +26,8 @@ staff(n20243,nurse,circulating,[typ1,typ2,typ3]).
 staff(t20241,technician,medical_Action,[typ1,typ2,typ3]).
 
 surgery(typ1,45,60,30).
+surgery(typ2,60,90,45).
+surgery(typ3,90,120,60).
 
 required_staff(typ1,doctor,orthopaedics,3,false,true,false).
 required_staff(typ1,doctor,anaesthesiology,1,true,true,false).
@@ -33,8 +35,20 @@ required_staff(typ1,nurse,instrumenting,1,false,true,false).
 required_staff(typ1,nurse,anaesthesiology,1,true,true,false).
 required_staff(typ1,nurse,circulating,1,false,true,false).
 required_staff(typ1,technician,medical_Action,1,false,true,true).
+required_staff(typ2,doctor,orthopaedics,3,false,true,false).
+required_staff(typ2,doctor,anaesthesiology,1,true,true,false).
+required_staff(typ2,nurse,instrumenting,1,false,true,false).
+required_staff(typ2,nurse,anaesthesiology,1,true,true,false).
+required_staff(typ2,nurse,circulating,1,false,true,false).
+required_staff(typ2,technician,medical_Action,1,false,true,true).
+required_staff(typ3,doctor,orthopaedics,2,false,true,false).
+required_staff(typ3,doctor,anaesthesiology,1,true,true,false).
+required_staff(typ3,nurse,instrumenting,1,false,true,false).
+required_staff(typ3,nurse,anaesthesiology,1,true,true,false).
+required_staff(typ3,nurse,circulating,1,false,true,false).
+required_staff(typ3,technician,medical_Action,1,false,true,true).
 
 surgery_id(req3,typ1).
 surgery_id(req4,typ1).
 
-agenda_operation_room(or1,20260810,[(100,180,ap1),(1080,1140,ap2)]).
+agenda_operation_room(or1,20260810,[(100,235,ap1),(1080,1215,ap2)]).
