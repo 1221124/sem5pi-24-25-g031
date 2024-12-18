6e380cff631576c45773f81ba9daa94b56048d1b
1200347 <1200347@isep.ipp.pt>
2024-12-18 16:49:29 +0000

    #83 Create CreatingSpecializationDto

---
+++ sarm/src/Domain/Specializations/CreatingSpecializationDto.cs
@@ -0,0 +1,15 @@
+﻿using Domain.Shared;
+
+namespace DDDNetCore.Domain.Specializations;
+
+public class CreatingSpecializationDto
+{
+    public Name Name { get; set; }
+    public Description Description { get; set; }
+
+    public CreatingSpecializationDto(Name name, Description description)
+    {
+        Name = name;
+        Description = description;
+    }
+}
