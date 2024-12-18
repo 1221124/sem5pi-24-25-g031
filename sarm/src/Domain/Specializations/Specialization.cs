0c351f4baa3bd20851ed1fbc7e4572c336d9eee1
1200347 <1200347@isep.ipp.pt>
2024-12-18 16:50:18 +0000

    #83 Create Specialization class

---
+++ sarm/src/Domain/Specializations/Specialization.cs
@@ -0,0 +1,26 @@
+﻿using Domain.Shared;
+
+namespace DDDNetCore.Domain.Specializations;
+
+public class Specialization : Entity<SpecializationId>, IAggregateRoot
+{
+    public SNOMEDCTCode SNOMEDCTCode { get; set; }
+    public Name Name { get; set; }
+    public Description Description { get; set; }
+
+    public Specialization(SNOMEDCTCode snomedctCode, Name name, Description description)
+    {
+        Id = new SpecializationId(Guid.NewGuid());
+        SNOMEDCTCode = snomedctCode;
+        Name = name;
+        Description = description;
+    }
+    
+    public Specialization(SpecializationId id, SNOMEDCTCode snomedCode, Name name, Description description)
+    {
+        Id = id;
+        SNOMEDCTCode = snomedCode;
+        Name = name;
+        Description = description;
+    }
+}
