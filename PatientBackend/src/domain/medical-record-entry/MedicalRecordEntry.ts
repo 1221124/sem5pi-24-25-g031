dd424da9e4d1639d1a947803f444ff45423003e7
Tiago Carvalho <1221124@isep.ipp.pt>
2024-12-19 21:36:22 +0000

    #116 Update medical record entry class

--- PatientBackend/src/domain/medical-record-entry/MedicalRecordEntry.ts
+++ PatientBackend/src/domain/medical-record-entry/MedicalRecordEntry.ts
@@ -4,6 +4,7 @@ import {ValueObject} from "../../core/domain/ValueObject";
 export class MedicalRecordEntryProps {
     code: ICD11Code;
     date: Date;
+    notMeaningfulAnyMore: boolean;
 }
 
 export class MedicalRecordEntry extends ValueObject<MedicalRecordEntryProps> {
@@ -14,12 +15,30 @@ export class MedicalRecordEntry extends ValueObject<MedicalRecordEntryProps> {
     get date (): Date {
         return this.props.date;
     }
+
+    set date (date: Date) {
+        this.props.date = date;
+    }
+
+    get notMeaningfulAnyMore (): boolean {
+        return this.props.notMeaningfulAnyMore;
+    }
+
+    set notMeaningfulAnyMore (notMeaningfulAnyMore: boolean) {
+        this.props.notMeaningfulAnyMore = notMeaningfulAnyMore;
+    }
     
     constructor(props: MedicalRecordEntryProps) {
         super(props);
     }
 
     public static create(arg0: ICD11Code, arg1: Date) {
-        return new MedicalRecordEntry({code: arg0, date: arg1});
+        return new MedicalRecordEntry({code: arg0, date: arg1, notMeaningfulAnyMore: false});
+    }
+
+    public toggleNotMeaningfulAnyMore(notMeaningfulAnyMore: boolean) {
+        if (this.props.notMeaningfulAnyMore !== notMeaningfulAnyMore) {
+            this.notMeaningfulAnyMore = notMeaningfulAnyMore;
+        }
     }
 }
