bcff5f26de6f2d2a1db8c774f9dcde2dce8eee30
1200347 <1200347@isep.ipp.pt>
2024-12-19 12:30:57 +0000

    #83 Create MedicalRecordEntry class

---
+++ PatientBackend/src/domain/medical-record-entry/MedicalRecordEntry.ts
@@ -0,0 +1,23 @@
+﻿import {ICD11Code} from "../shared/ICD11Code";
+import {AggregateRoot} from "../../core/domain/AggregateRoot";
+import {ValueObject} from "../../core/domain/ValueObject";
+
+export class MedicalRecordEntryProps {
+    code: ICD11Code;
+    date: Date;
+}
+
+export class CommonSymptom extends ValueObject<MedicalRecordEntryProps> {
+
+    get code (): ICD11Code {
+        return this.props.code;
+    }
+
+    get date (): Date {
+        return this.props.date;
+    }
+    
+    constructor(props: MedicalRecordEntryProps) {
+        super(props);
+    }
+}
