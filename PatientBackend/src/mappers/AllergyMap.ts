5994e28efec0d23f2c524e02d90b8148dd34b09d
1220786 <1220786@isep.ipp.pt>
2024-12-21 16:30:00 +0000

    #112 BE Patient Allergy Update Mapper
    added fromPersintence method to allergyMap class

--- PatientBackend/src/mappers/AllergyMap.ts
+++ PatientBackend/src/mappers/AllergyMap.ts
@@ -3,7 +3,6 @@ import {AllergyDto} from "../dto/allergy/AllergyDto";
 import {ICD11Code} from "../domain/shared/ICD11Code";
 import {Name} from "../domain/shared/Name";
 import {Description} from "../domain/shared/Description";
-import {UniqueEntityID} from "../core/domain/UniqueEntityID";
 import {AllergyId} from "../domain/allergy/AllergyId";
 
 export class AllergyMap {
@@ -53,5 +52,27 @@ export class AllergyMap {
             name: allergy.name.value,
             description: allergy.description.value
         };
+    }
+
+    public static fromPersistence (raw: any): Allergy {
+        const allergyId = AllergyId.create(raw.allergyId.toString());
+
+        const codeOrError = ICD11Code.create(raw.code);
+        if (codeOrError.isFailure) throw new Error("ICD11 code failed validation.");
+        const code = codeOrError.getValue();
+
+        const nameOrError = Name.create(raw.name);
+        if (nameOrError.isFailure) throw new Error("Name failed validation.");
+        const name = nameOrError.getValue();
+
+        const descriptionOrError = Description.create(raw.description);
+        if (descriptionOrError.isFailure) throw new Error("Description failed validation.");
+        const description = descriptionOrError.getValue();
+
+        return Allergy.create({
+            code: code,
+            name: name,
+            description: description
+        }, allergyId.id).getValue();
     }
 }
