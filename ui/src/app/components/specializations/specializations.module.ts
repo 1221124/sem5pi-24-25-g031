d7ac821b30ec2c3448c9b4ddef22908ffe9576e3
1200347 <1200347@isep.ipp.pt>
2024-12-22 18:44:53 +0000

    #83 Specialization module file created

---
+++ ui/src/app/components/specializations/specializations.module.ts
@@ -0,0 +1,26 @@
+﻿import { NgModule } from '@angular/core';
+import { CommonModule, NgFor, NgIf } from '@angular/common';
+import {FormsModule} from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import {SpecializationsComponent} from "./main-specialization/specializations.component";
+import {routes} from "../../app.routes";
+import {AddSpecializationComponent} from "./add-specialization/add-specialization.component";
+import {ListSpecializationComponent} from "./list-specialization/list-specialization.component";
+import {SpecializationsService} from "../../services/specializations/specializations.service";
+
+@NgModule({
+    imports: [
+        RouterModule.forChild(routes),
+        CommonModule,
+        FormsModule,
+        NgFor,
+        NgIf,
+        SpecializationsComponent,
+        AddSpecializationComponent,
+        ListSpecializationComponent
+    ],
+    exports: [
+        SpecializationsComponent
+    ]
+})
+export class SpecializationsModule { }
