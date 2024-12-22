8a56e27b70ac29b0886b675502313e7d3fe08979
1200347 <1200347@isep.ipp.pt>
2024-12-22 21:22:46 +0000

    #87 List specialization updated

--- ui/src/app/components/specializations/list-specialization/list-specialization.component.ts
+++ ui/src/app/components/specializations/list-specialization/list-specialization.component.ts
@@ -1,6 +1,10 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import {OperationType} from '../../../models/operation-type.model';
+import {Specialization} from '../../../models/specialization.model';
+import {AuthService} from '../../../services/auth/auth.service';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-list-specialization',
@@ -9,6 +13,37 @@ import { FormsModule } from '@angular/forms';
   templateUrl: './list-specialization.component.html',
   styleUrl: './list-specialization.component.css'
 })
-export class ListSpecializationComponent {
+export class ListSpecializationComponent implements OnInit {
+  @Input() specializations: Specialization[] = [];
+  @Input() currentPage: number = 1;
+  @Input() itemsPerPage: number = 1;
+  accessToken = '';
 
+  constructor(
+    private authService: AuthService,
+    private router: Router
+  ) {}
+
+  async ngOnInit() {
+    if (!this.authService.isAuthenticated()) {
+      this.authService.updateMessage('You are not authenticated or are not an admin! Please login...');
+      this.authService.updateIsError(true);
+      this.router.navigate(['']);
+      return;
+    }
+
+    this.accessToken = this.authService.getToken() as string;
+
+    if (!this.authService.extractRoleFromAccessToken(this.accessToken)?.toLowerCase().includes('admin')) {
+      this.authService.updateMessage('You are not authenticated or are not an admin! Redirecting to login...');
+      this.authService.updateIsError(true);
+      this.router.navigate(['']);
+      return;
+    }
+  }
+
+  getPaginatedSpecializations() : Specialization[] {
+    const start = (this.currentPage - 1) * this.itemsPerPage;
+    return this.specializations.slice(start, start + this.itemsPerPage);
+  }
 }
