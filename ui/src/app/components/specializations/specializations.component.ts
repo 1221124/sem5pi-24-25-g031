c5d87c6dc30486b6788716afd5e0b241c00fc7b5
1200347 <1200347@isep.ipp.pt>
2024-12-22 02:48:18 +0000

    #83 Update main page to specialization

--- ui/src/app/components/specializations/specializations.component.ts
+++ ui/src/app/components/specializations/specializations.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import {Component, Output} from '@angular/core';
+import {StaffsService} from '../../services/staffs/staffs.service';
+import {AuthService} from '../../services/auth/auth.service';
+import {ActivatedRoute, Router} from '@angular/router';
+import {Staff} from '../../models/staff.model';
 
 @Component({
   selector: 'app-specializations',
@@ -6,5 +10,31 @@ import { Component } from '@angular/core';
   styleUrl: './specializations.component.css'
 })
 export class SpecializationsComponent {
+  @Output() selectedSpecializationToCreate;
+
+  isCreateModalOpen = false;
+  closeModal() {
+    this.isCreateModalOpen = false;
+  }
+
+  constructor(private authService: AuthService, private router: Router, private route: ActivatedRoute) { }
+
+  openModal() {
+    console.log('Opening create modal...');
+
+    this.isCreateModalOpen = true;
+
+    this.navigateTo('create', { queryParams: { request: JSON.stringify(this.selectedSpecializationToCreate) } });
+  }
+
+  navigateTo(route: string, options?: { queryParams?: any }) {
+    this.router
+      .navigate([route], {
+        relativeTo: this.route,
+        queryParams: options?.queryParams,
+      })
+      .then(r => console.log('Navigated to:', r))
+      .catch(err => console.error('Navigation Error:', err));
+  }
 
 }
