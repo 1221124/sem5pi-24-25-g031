6c3684690db2cbb288081c410ecac27b3775bbb3
1200347 <1200347@isep.ipp.pt>
2024-12-22 01:25:01 +0000

    #83 Database created
    SpecializationEntityTypeConfiguration and SpecializationRepository files created

---
+++ sarm/src/Infrastructure/Specializations/SpecializationEntityTypeConfiguration.cs
@@ -0,0 +1,39 @@
+﻿using DDDNetCore.Domain.Specializations;
+using Domain.Shared;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Specialization = DDDNetCore.Domain.Specializations.Specialization;
+
+namespace DDDNetCore.Infrastructure.Specializations;
+
+public class SpecializationEntityTypeConfiguration : IEntityTypeConfiguration<Specialization>
+{
+    public void Configure(EntityTypeBuilder<Specialization> builder)
+    {
+        builder.HasKey(x => x.Id);
+        
+        builder.Property(x => x.SNOMEDCTCode)
+            .IsRequired()
+            .HasColumnName("SNOMEDCTCode")
+            .HasConversion(
+                v => v.Value,
+                v => new SNOMEDCTCode(v)
+            );
+
+        builder.Property(x => x.Name)
+            .IsRequired()
+            .HasColumnName("Name")
+            .HasConversion(
+                v => v.Value,
+                v => new Name(v)
+            );
+
+        builder.Property(x => x.Description)
+            .IsRequired()
+            .HasColumnName("Description")
+            .HasConversion(
+                v => v.Value,
+                v => new Description(v)
+            );
+    }
+}
---
+++ sarm/src/Infrastructure/Specializations/SpecializationRepository.cs
@@ -0,0 +1,44 @@
+﻿using DDDNetCore.Domain.Specializations;
+using Domain.Shared;
+using Infrastructure;
+using Infrastructure.Shared;
+using Microsoft.EntityFrameworkCore;
+using Specialization = DDDNetCore.Domain.Specializations.Specialization;
+
+namespace DDDNetCore.src.Infrastructure.Specializations{
+
+    public class SpecializationRepository : BaseRepository<Specialization, SpecializationId>, ISpecializationRepository
+    {
+        private DbSet<Specialization> _objs;
+
+        public SpecializationRepository(SARMDbContext context) : base(context.Specializations)
+        {
+            _objs = context.Set<Specialization>();
+        }
+
+        public Task<Specialization> GetByCodeAsync(SNOMEDCTCode snomedctCode)
+        {
+            return _objs.FirstOrDefaultAsync(x => x.SNOMEDCTCode == snomedctCode);
+        }
+
+        public Task<List<Specialization>> GetByNameAsync(Name name)
+        {
+            return _objs.Where(x => x.Name == name).ToListAsync();
+        }
+
+        public Task<List<Specialization>> GetByDescriptionAsync(Description description)
+        {
+            return _objs.Where(x => x.Description == description).ToListAsync();
+        }
+
+        public async Task<string?> GetLastCodeAsync()
+        {
+            var last = await _objs
+                .AsQueryable()
+                .OrderByDescending(x => x.SNOMEDCTCode)
+                .FirstOrDefaultAsync();
+
+            return last?.SNOMEDCTCode;
+        }
+    }
+}
