8e482d21049c93368b29768ec0b150805914fa4e
1200347 <1200347@isep.ipp.pt>
2024-12-22 18:46:40 +0000

    #83 Create specialization updated

--- ui/src/app/components/specializations/add-specialization/add-specialization.component.ts
+++ ui/src/app/components/specializations/add-specialization/add-specialization.component.ts
@@ -1,7 +1,11 @@
-import {Component, EventEmitter, Input, Output} from '@angular/core';
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
 import {Staff} from '../../../models/staff.model';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import {Specialization} from '../../../models/specialization.model';
+import {StaffsService} from '../../../services/staffs/staffs.service';
+import {AuthService} from '../../../services/auth/auth.service';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-add-specialization',
@@ -10,22 +14,65 @@ import { FormsModule } from '@angular/forms';
   templateUrl: './add-specialization.component.html',
   styleUrl: './add-specialization.component.css'
 })
-export class AddSpecializationComponent {
+export class AddSpecializationComponent implements OnInit{
+
+  @Input() specialization!: Specialization;
+
 
   @Output() closeModalEvent = new EventEmitter<unknown>();
 
   nameTouched = false;
   descriptionTouched = false;
+  codeTouched = false;
+  @Output() createSpecializationEvent = new EventEmitter<Specialization>();
+
+  accessToken: string = '';
+  constructor(private authService: AuthService, private router: Router) {
+  }
+
+  ngOnInit(): void {
+
+    if (!this.specialization) {
+      this.specialization = {
+        Id: '',
+        SNOMEDCTCode: '',
+        Name: '',
+        Description: ''
+      };
+    }
+
+    if (!this.authService.isAuthenticated()) {
+      this.authService.updateMessage('You are not authenticated or are not an admin! Please login...');
+      this.authService.updateIsError(true);
+      this.router.navigate(['']);
+      return;
+    }
+
+    this.accessToken = this.authService.getToken() as string;
+    if (!this.authService.extractRoleFromAccessToken(this.accessToken)?.toLowerCase().includes('admin')) {
+      this.authService.updateMessage(
+        'You are not authenticated or are not an admin! Redirecting to login...'
+      );
+      this.authService.updateIsError(true);
+      this.router.navigate(['']);
+      return;
+    }
+  }
 
   closeCreateModal() {
     this.closeModalEvent.emit();
   }
 
   submitRequest() {
-
+    this.createSpecializationEvent.emit(this.specialization);
   }
 
   clearForm() {
-
+    this.codeTouched = false;
+    this.nameTouched = false;
+    this.descriptionTouched = false;
   }
+
+
+
 }
