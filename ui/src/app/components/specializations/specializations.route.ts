1dc53d40280369a07fb7f01b8a24cded267e04a8
1200347 <1200347@isep.ipp.pt>
2024-12-22 18:45:09 +0000

    #83 Specialization route file created

---
+++ ui/src/app/components/specializations/specializations.route.ts
@@ -0,0 +1,15 @@
+﻿import { Route, RouterModule } from '@angular/router';
+import { NgModule } from '@angular/core';
+import {AddSpecializationComponent} from './add-specialization/add-specialization.component';
+import {SpecializationsComponent} from './main-specialization/specializations.component';
+
+export const routes: Route[] = [
+  { path: '', component: SpecializationsComponent },
+  { path: 'create', component: AddSpecializationComponent }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
