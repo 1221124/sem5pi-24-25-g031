df6fd79897c68fdf36d2197df64200b759f697ca
1220786 <1220786@isep.ipp.pt>
2024-12-26 23:08:14 +0000

    #74 #75 #112 FE Patient Allergy Update
    added feature to update a allergy, and added styling to it

--- ui/src/app/components/allergy-module/update-allergy/update-allergy.component.ts
+++ ui/src/app/components/allergy-module/update-allergy/update-allergy.component.ts
@@ -1,11 +1,59 @@
-import { Component } from '@angular/core';
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+import {NgIf} from '@angular/common';
+import {FormsModule} from '@angular/forms';
+import {Allergy} from '../../../models/allergy.model';
 
 @Component({
   selector: 'app-update-allergy',
   templateUrl: './update-allergy.component.html',
   standalone: true,
+  imports: [
+    NgIf,
+    FormsModule
+  ],
   styleUrl: './update-allergy.component.css'
 })
-export class UpdateAllergyComponent {
+export class UpdateAllergyComponent implements OnInit{
+  @Input() allergy!: Allergy;
+
+  @Output() close = new EventEmitter<unknown>();
+  @Output() update = new EventEmitter<Allergy>();
+
+  isProcessing: boolean = false;
+
+  message: string = '';
+  success: boolean = false;
+
+  updatedDescription: string = '';
+
+  ngOnInit() {
+    if (!this.allergy) {
+      console.error('allergy is not defined.');
+      return;
+    }
+
+    this.updatedDescription = this.allergy.description;
+  }
+
+  submit() {
+    this.isProcessing = true;
+    this.success = false;
+    this.message = 'Updating allergy...';
+
+
+    if(this.updatedDescription !== this.allergy.description && this.updatedDescription != '') this.allergy.description = this.updatedDescription;
+
+    this.isProcessing = false;
+    this.success = true;
+    this.message = 'Allergy updated successfully!';
+
+    this.update.emit(this.allergy);
+  }
+
+  adjustHeight(event: Event): void {
+    const textarea = event.target as HTMLTextAreaElement;
+    textarea.style.height = 'auto';
+    textarea.style.height = `${textarea.scrollHeight}px`;
+  }
 
 }
