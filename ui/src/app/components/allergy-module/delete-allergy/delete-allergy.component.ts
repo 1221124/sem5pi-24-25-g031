a6085f4025d690d33b0a707700ff3bee5c33405f
1220786 <1220786@isep.ipp.pt>
2024-12-26 12:10:38 +0000

    #74 #75 #112 FE patient Allergy Delete
    added classes to be able to delete allergies

---
+++ ui/src/app/components/allergy-module/delete-allergy/delete-allergy.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-delete-allergy',
+  templateUrl: './delete-allergy.component.html',
+  standalone: true,
+  styleUrl: './delete-allergy.component.css'
+})
+export class DeleteAllergyComponent {
+
+}
