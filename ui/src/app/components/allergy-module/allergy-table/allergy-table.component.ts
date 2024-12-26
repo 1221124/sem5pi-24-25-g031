d18c3a1df57028e5a2f2e4e6268f01fb5aa2f53a
1220786 <1220786@isep.ipp.pt>
2024-12-26 12:00:36 +0000

    #74 #75 #112 FE Patient Allergy Table
    created classes to have a alelerfy tables

---
+++ ui/src/app/components/allergy-module/allergy-table/allergy-table.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-allergy-table',
+  templateUrl: './allergy-table.component.html',
+  standalone: true,
+  styleUrl: './allergy-table.component.css'
+})
+export class AllergyTableComponent {
+
+}
