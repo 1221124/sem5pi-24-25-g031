ad554aebddbaf342387ec211bfaf04037ca95bfa
1220786 <1220786@isep.ipp.pt>
2024-12-26 23:28:16 +0000

    #74 #75 #112 FE Patient Allergy Delete
    added method for delete allergy, and styling

--- ui/src/app/components/allergy-module/delete-allergy/delete-allergy.component.ts
+++ ui/src/app/components/allergy-module/delete-allergy/delete-allergy.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import {Component, EventEmitter, Input, Output, output} from '@angular/core';
+import {Allergy} from '../../../models/allergy.model';
+import {AuthService} from '../../../services/auth/auth.service';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-delete-allergy',
@@ -7,5 +10,40 @@ import { Component } from '@angular/core';
   styleUrl: './delete-allergy.component.css'
 })
 export class DeleteAllergyComponent {
+  @Input() allergy!: Allergy;
 
+  @Output() confirmDeleteEvent = new EventEmitter();
+  @Output() closeDeleteEvent = new EventEmitter();
+
+  constructor(
+    private authService: AuthService,
+    private router: Router
+  ) {}
+
+  accessToken: string = '';
+
+  async ngOnInit() {
+    if (!this.authService.isAuthenticated()) {
+      this.authService.updateMessage('You are not authenticated or are not an admin! Please login...');
+      this.authService.updateIsError(true);
+      await this.router.navigate(['']);
+      return;
+    }
+
+    this.accessToken = this.authService.getToken() as string;
+    if (!this.authService.extractRoleFromAccessToken(this.accessToken)?.toLowerCase().includes('admin')) {
+      this.authService.updateMessage('You are not an admin! Redirecting to login...');
+      this.authService.updateIsError(true);
+      await this.router.navigate(['']);
+      return;
+    }
+  }
+
+  confirmDelete() {
+    this.confirmDeleteEvent.emit(this.allergy);
+  }
+
+  closeDelete() {
+    this.closeDeleteEvent.emit();
+  }
 }
