da881f60676fc06232dab200daadf1a072961ed4
1220786 <1220786@isep.ipp.pt>
2024-12-26 12:10:53 +0000

    #74 #75 #112 FE patient Allergy Update
    added classes to be able to update classes

---
+++ ui/src/app/components/allergy-module/update-allergy/update-allergy.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-update-allergy',
+  templateUrl: './update-allergy.component.html',
+  standalone: true,
+  styleUrl: './update-allergy.component.css'
+})
+export class UpdateAllergyComponent {
+
+}
