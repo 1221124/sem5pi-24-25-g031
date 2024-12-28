7adfbeb112c4c2557b1fc35a144a955381270bec
1200347 <1200347@isep.ipp.pt>
2024-12-28 18:50:28 +0000

    #78 #79 IAllergyService added

--- PatientBackend/src/services/IServices/IAllergyService.ts
+++ PatientBackend/src/services/IServices/IAllergyService.ts
@@ -7,4 +7,5 @@ export default interface IAllergyService  {
     createAllergy(dto: CreatingAllergyDto): Promise<Result<AllergyDto>>;
     getAll(filters: {code?: string, name?: string, description?: string}): Promise<AllergyDto[]>;
     updateAllergy(id: string, dto: UpdatingAllergyDto): Promise<Result<AllergyDto>>;
+    validateICD11Code(code: string): Promise<Result<boolean>>;
 }
