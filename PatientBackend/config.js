24eb8a69cdc9d5345a70e845c4c3d8be6ee857b2
David Miguel de Oliveira Costa Sousa <1220784@isep.ipp.pt>
2024-12-28 00:28:47 +0000

    #1 Patient Backend Tests configuration done

--- PatientBackend/config.js
+++ PatientBackend/config.js
@@ -1,4 +1,4 @@
-import dotenv from 'dotenv';
+const dotenv = require('dotenv');
 
 // Set the NODE_ENV to 'development' by default
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
@@ -10,7 +10,7 @@ if (!envFound) {
   throw new Error("⚠️  Couldn't find .env file  ⚠️");
 }
 
-export default {
+module.exports = {
   /**
    * Your favorite port : optional change to 4000 by JRT
    */
