b667479637694f98ccfa7b861fe18aeabbceaba2
1200347 <1200347@isep.ipp.pt>
2024-12-29 20:37:53 +0000

    #85 UpdatingSpecializationDto file added

---
+++ sarm/src/Domain/Specializations/UpdatingSpecializationDto.cs
@@ -0,0 +1,16 @@
+﻿using Domain.Shared;
+
+namespace DDDNetCore.Domain.Specializations;
+
+public class UpdatingSpecializationDto
+{
+    public Name Name { get; set; }
+    
+    public Description Description { get; set; }
+
+    public UpdatingSpecializationDto(Name name, Description description)
+    {
+        Name = name;
+        Description = description;
+    }
+}
