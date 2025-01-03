9c7ab3c10bf3b5d9f91fd8570cb05c5c00e3aa19
1220786 <1220786@isep.ipp.pt>
2025-01-03 11:56:44 +0000

    #74 #75 #112 BE Patient Allergy CreatingDto
    change create method in CreatintAllergyDto class

--- PatientBackend/src/dto/allergy/CreatingAllergyDto.ts
+++ PatientBackend/src/dto/allergy/CreatingAllergyDto.ts
@@ -1,6 +1,7 @@
 import {ICD11Code} from "../../domain/shared/ICD11Code";
 import {Name} from "../../domain/shared/Name";
 import {Description} from "../../domain/shared/Description";
+import {Result} from "../../core/logic/Result";
 
 export class CreatingAllergyDto {
     code: ICD11Code;
@@ -13,10 +14,17 @@ export class CreatingAllergyDto {
         this.description = description;
     }
 
-    public static create (code: string, name: string, description: string): CreatingAllergyDto {
-        const icd11Code = ICD11Code.create(code).getValue();
-        const allergyName = Name.create(name).getValue();
-        const allergyDescription = Description.create(description).getValue();
-        return new CreatingAllergyDto(icd11Code, allergyName, allergyDescription);
+    public static create (code: string, name: string, description: string): Result<CreatingAllergyDto> {
+        const icd11Code = ICD11Code.create(code);
+        const allergyName = Name.create(name);
+        const allergyDescription = Description.create(description);
+
+        const creatingAllergyDto = new CreatingAllergyDto(
+            icd11Code.getValue(),
+            allergyName.getValue(),
+            allergyDescription.getValue()
+        );
+
+        return Result.ok<CreatingAllergyDto>(creatingAllergyDto);
     }
 }
