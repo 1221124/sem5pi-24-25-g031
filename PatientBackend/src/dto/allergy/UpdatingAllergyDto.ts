6e670ff858c448b6eb8fda80ad7e1ac7ab016e8f
1220786 <1220786@isep.ipp.pt>
2025-01-03 14:41:34 +0000

    #74 #75 #112 BE Patient Allergy Updating Dto
    changed create method

--- PatientBackend/src/dto/allergy/UpdatingAllergyDto.ts
+++ PatientBackend/src/dto/allergy/UpdatingAllergyDto.ts
@@ -1,4 +1,5 @@
 import {Description} from "../../domain/shared/Description";
+import {Result} from "../../core/logic/Result";
 
 export class UpdatingAllergyDto {
     description: Description;
@@ -7,7 +8,12 @@ export class UpdatingAllergyDto {
         this.description = description ?? Description.create(null).getValue();
     }
 
-    public static create(description: Description): UpdatingAllergyDto {
-        return new UpdatingAllergyDto(description);
+    public static create(description: string): Result<UpdatingAllergyDto> {
+        const AllergyDescription = Description.create(description);
+
+        const updatingAllergyDto = new UpdatingAllergyDto(
+            AllergyDescription.getValue()
+        );
+        return Result.ok<UpdatingAllergyDto>(updatingAllergyDto);
     }
 }
